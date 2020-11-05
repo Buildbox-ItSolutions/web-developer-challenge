4774cec540df38f1a196630c19952278ff8222f2
Jonathan-Rios <jonathan.riosousa@gmail.com>
2020-11-05 12:16:26 -0300

    lastest

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/global.css";
+
+import Header from "./components/Header";
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
