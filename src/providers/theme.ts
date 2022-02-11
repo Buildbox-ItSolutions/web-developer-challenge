6712acc2d2b7f146aa084d0bffbf1dcea8302bec
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-11 00:56:34 -0300

    styled components added

---
+++ src/providers/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        primary: '#00bcd4',
+        secondary: '#ff9800',
+        neutral: '#fafafa',
+        neutralalt: '#f5f5f5',
+    },
+    breakpoints: {
+        xs: '576px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+    }
+}
