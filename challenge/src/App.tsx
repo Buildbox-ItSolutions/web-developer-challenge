3eb544dbe94d04029821c1c7c7fdec461c86241b
Joao Victor Longo <joaovictorlongoo@gmail.com>
2022-02-12 11:08:15 -0300

    feat: add Header component

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -1,9 +1,10 @@
 import GlobalStyle from './styles/global'
+import { Header } from './components/Header'
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Header />
       <GlobalStyle />
     </div>
   )
