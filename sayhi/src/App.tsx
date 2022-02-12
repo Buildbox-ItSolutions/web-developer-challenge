7d1446c869a59cef32f61bb8d27c92d0d8d4326e
Joao Victor Longo <joaovictorlongoo@gmail.com>
2022-02-11 21:07:38 -0300

    feat: add global style

--- sayhi/src/App.tsx
+++ sayhi/src/App.tsx
@@ -1,5 +1,10 @@
-import './App.css'
+import GlobalStyle from './styles/global'
 
 export function App() {
-  return <h1>Hello World</h1>
+  return (    
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  )
 }
