c3d177d54032bcc4a5bfb5c10bba681774a4e7f9
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 13:07:49 -0300

    🧪 created App initial test file

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/react app/i)
-  expect(linkElement).toBeInTheDocument()
 })
