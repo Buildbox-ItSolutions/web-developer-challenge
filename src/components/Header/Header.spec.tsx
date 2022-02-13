5aca8e0389bed0ef79346add18a8446327ec7382
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 12:44:29 -0300

    🧪 Header component initial test file created

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Header from '.'
+
+describe('<Header />', () => {
+  it('dummy test', () => {
+    render(<Header />)
+  })
+})
