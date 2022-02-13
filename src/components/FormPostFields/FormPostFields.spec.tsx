ed7f3bd4719643ecc1dc860995fd76c3a4a37db3
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 16:55:30 -0300

    🔥 Renaming FormPostFields component

--- src/components/FormPostFields/FormPostFields.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import FormPostFields from '.'
-
-describe('<FormPostFields', () => {
-  it('dummy test', () => {
-    render(<FormPostFields />)
-  })
-})
