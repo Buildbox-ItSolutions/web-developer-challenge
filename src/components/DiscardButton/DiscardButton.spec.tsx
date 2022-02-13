80def9bfed239a52bac4286160668bf071974e89
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 12:54:20 -0300

    🧪 created initial test file of DiscardButton component

---
+++ src/components/DiscardButton/DiscardButton.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import DiscardButton from '.'
+
+describe('<DiscardButton />', () => {
+  it('dummy test', () => {
+    render(<DiscardButton />)
+  })
+})
