2f02288c7812c3927038428bd12ee2c793abbcb3
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 12:51:10 -0300

    PublicButton component initial test file created

---
+++ src/components/PublicButton/PublicButton.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import PublicButton from '.'
+
+describe('<PublicButton />', () => {
+  it('dummy test', () => {
+    render(<PublicButton />)
+  })
+})
