4802e19249a1bd5556bb3273349bfc549366c023
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 17:09:17 -0300

    🧪 Feed component initial test file created

---
+++ src/components/Feed/Feed.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Feed from '.'
+
+describe('<Feed />', () => {
+  it('dummy test', () => {
+    render(<Feed />)
+  })
+})
