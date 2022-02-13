10ef3246c6bd208b9af6f6d3c2485d86bf984740
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 12:53:03 -0300

    💄 created the DiscardButton component styles

---
+++ src/components/DiscardButton/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.button`
+  width: 60px;
+  padding: 16px;
+
+  position: relative;
+
+  cursor: pointer;
+  transition: ease-in-out 0.3s;
+
+  font-size: 14px;
+  font-weight: normal;
+
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.gray};
+
+  &::after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    display: block;
+
+    top: 32px;
+    position: absolute;
+
+    background-color: ${({ theme }) => theme.colors.gray};
+  }
+
+  &:hover {
+    opacity: 0.9;
+    transform: scale(1.05);
+  }
+`
