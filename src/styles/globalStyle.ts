0494da71e5fadf050cb3650abad22c45fca4c88c
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 16:59:25 -0300

    🎨 Formatting code

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,30 +1,37 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-    * {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
   html {
     font-size: 62.5%;
   }
+
   html, body, #__next  {
     height: 100%;
   }
+
   body {
     font-family: 'Roboto', sans-serif;
     background-color: ${({ theme }) => theme.colors.blackLight};
   }
+
   a {
     text-decoration: none;
   }
+
   ul {
     list-style: none;
   }
+
   button {
     border: none;
   }
+
   input {
     border: none;
     outline: none;
