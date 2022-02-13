3604a4cd42ac2a65c5b6e04b397f67ff1cfc1e36
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 13:09:08 -0300

    ✨ wrapping the App in ThemeProvider, and adding the Main component to be rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import defaultTheme from './theme/defaultTheme'
 import { GlobalStyles } from './styles/globalStyle'
+import Main from './components/Main'
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>React App</h1>
-    </>
+      <Main />
+    </ThemeProvider>
   )
 }
 
