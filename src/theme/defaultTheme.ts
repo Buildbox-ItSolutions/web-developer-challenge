2953298afa6f3ac8a0bcefdb08e00485ce23083d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 17:04:36 -0300

    ⚰️ removing comments

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -1,23 +1,15 @@
 export default {
   colors: {
-    //White
     white: '#ffffff',
-    //Black
     blackDark: '#2b2b2b',
     black: '#313131',
     blackLight: '#343434',
-
-    //Border colors
     borderColor: '#3b3b3b',
-
-    //Gray
     grayDark1: '#494949',
     grayDark2: '#7a7a7a',
     gray: '#5f5f5f',
     grayLight: '#9f9f9f',
-
-    //Auxiliary Colors
-    green: '#71bb00',
+    bgButton: '#71bb00',
     red: '#a62502'
   }
 }
