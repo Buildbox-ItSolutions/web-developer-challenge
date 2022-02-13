8d561310fa5749c58999cafe71df2dd9f16b4e7d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 17:11:39 -0300

    ♻️ Refactoring the Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,40 +1,15 @@
 import React from 'react'
-import FormPostFields from '../FormPostFields'
+import Feed from '../Feed'
+import Form from '../Form'
 import Header from '../Header'
-import Post from '../Post'
 import * as S from './style'
-
-const dataExemple = [
-  {
-    id: 1,
-    photo: '/image/image.png',
-    title: 'Lorem ipsum dolor sit amet',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.',
-    author: 'Manuela Oliveira'
-  },
-  {
-    id: 2,
-    photo: '/image/image.png',
-    title: 'Lorem ipsum dolor sit amet',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.',
-    author: 'Manuela Oliveira'
-  }
-]
 
 const Main: React.FC = () => {
   return (
     <S.Wrapper>
       <Header />
-      <FormPostFields />
-      {dataExemple.map(({ id, photo, title, body, author }) => (
-        <Post
-          key={id}
-          photo={photo}
-          title={title}
-          body={body}
-          author={author}
-        />
-      ))}
+      <Form />
+      <Feed />
     </S.Wrapper>
   )
 }
