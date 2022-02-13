2982d648fb6f8c8bd74b938d0f8299b53c64a32e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 17:05:27 -0300

    ⚰️ removing comments

--- src/theme/styles.td.ts
+++ src/theme/styles.td.ts
@@ -3,25 +3,16 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
-      //White
       white: string
-      //Black
       blackDark: string
       black: string
       blackLight: string
-
-      //Border colors
       borderColor: string
-
-      //Gray
       grayDark1: string
       grayDark2: string
       gray: string
       grayLight: string
-
-      //Auxiliary Colors
-      green: string
-      red: string
+      bgButton: string
     }
   }
 }
