fcbfbd4a788a205c81aaa4172b511b16c3ec4f63
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 13:03:21 -0300

    ⚡️ created a mock of the Post component props

---
+++ src/components/Post/mockProps.ts
@@ -0,0 +1,7 @@
+export const postProps = {
+  id: 1,
+  photo: '/image/image.png',
+  title: 'Image title',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.',
+  author: 'Manuela Oliveira'
+}
