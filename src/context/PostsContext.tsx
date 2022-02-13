36ebf691df2509087f816ad2d66f950457dc764d
Cipriano99 <samuelcipriano99@gmail.com>
2022-02-13 03:56:38 -0300

    Create PostsContext

---
+++ src/context/PostsContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, ReactNode, useState } from 'react';
+
+export type PostType = {
+  image: string;
+  author: string;
+  text: string;
+};
+
+type PostTypeContext = {
+  posts: PostType[];
+  setPosts: React.Dispatch<React.SetStateAction<PostType[]>>;
+};
+
+const defaultValues = {
+  posts: [],
+  setPosts: () => {},
+};
+export const PostsContext = createContext<PostTypeContext>(defaultValues);
+
+export const PostsProvider = ({ children }: { children: ReactNode }) => {
+  const [posts, setPosts] = useState<PostType[]>(defaultValues.posts);
+
+  return (
+    <PostsContext.Provider value={{ posts, setPosts }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { PostsProvider } from './context/PostsContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PostsProvider>
+      <App />
+    </PostsProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
