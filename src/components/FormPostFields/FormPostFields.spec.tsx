015e4ba17ac8aad86d2337dbd90a1f6cb952f66d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-13 12:47:41 -0300

    🧪 FormPostFields component initial test file created

---
+++ src/components/FormPostFields/FormPostFields.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import FormPostFields from '.'
+
+describe('<FormPostFields', () => {
+  it('dummy test', () => {
+    render(<FormPostFields />)
+  })
+})
