70943ce9af48a9b29177b3cff1151700e5bd4351
EvertonMirand <everton.mirandav@gmail.com>
2022-02-14 17:05:26 -0300

    Cria botão de deletar

---
+++ src/components/DeleteButton/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import trashImage from '../../images/delete.png';
+
+type Props = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const DeleteButton: React.FC<Props> = ({ onClick }) => {
+  return (
+    <Container onClick={onClick}>
+      <img src={trashImage} alt="excluir mensagem" />
+    </Container>
+  );
+};
+
+export default DeleteButton;
