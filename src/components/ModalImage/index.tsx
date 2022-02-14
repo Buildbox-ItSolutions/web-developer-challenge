e739d36c5404ecfd617682ae4d541ea267580a60
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-14 13:39:34 -0300

    Refactored Modal Image

--- src/components/ModalImage/index.tsx
+++ src/components/ModalImage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { Image } from '../index'
 import { ModalWrap } from './style'
 import PlaceholderImg from '../../assets/img/placeholderimg.svg';
@@ -8,24 +8,24 @@ import { getImages, uploadImage, getPublicUrl } from '../../services/supaFunctio
 export function ModalImage() {
     const [image, setImage] = useState(PlaceholderImg);
     const [isOpen, setIsOpen] = useState(false);
-    const [bg, setBg] = useState<any>();
     const [modalImages, setModalImages] = useState<any>([]);
-    const [namesImage, setNamesImage] = useState<any>();
+    const [urlImages, setUrlImages] = useState<any>();
 
     useEffect(() => {
-        async function aff() {
+        async function getAllImages() {
             const response = await getImages();
-            setNamesImage(response);
+            setUrlImages(response);
         }
-        aff()
+
+        getAllImages()
     }, [])
 
     useEffect(() => {
         const allImages: any = [];
 
-        async function teste() {
-            if(namesImage){
-                for (const image of namesImage) {
+        async function queryImages() {
+            if(urlImages){
+                for (const image of urlImages) {
 
                     const response = await getPublicUrl(image.name);
                     allImages.push(response);
@@ -35,22 +35,25 @@ export function ModalImage() {
             setModalImages(allImages);
         }
         
-        teste();
-
-    }, [namesImage]);
+        queryImages();
+    }, [urlImages]);
 
     return (
     <>
         <ModalWrap onClick={() => setIsOpen(!isOpen)}>
-            {modalImages && modalImages.map( (image: string) => {
-                    return ( <img key={image} src={image} alt=""/> )                
-            })}
+            <Image src={image} />
+         
             { isOpen && <div className="modal-backdrop">
                     <div className='modal-content'>
-                        {modalImages && modalImages.map( async (img: any) => {
-                            <div className='modal-img' key={img.name}>
-                            </div>
-                        })}
+                        <div className='modal-content__wrap'>
+                            {modalImages && modalImages.map( (image: string) => {
+                                return ( 
+                                    <div className='modal-content__image' key={image}>
+                                        <img src={image} alt=""/> 
+                                    </div>
+                                )                
+                            })}
+                        </div>
                     </div>
             </div>}
         </ModalWrap>
--- src/components/ModalImage/style.ts
+++ src/components/ModalImage/style.ts
@@ -23,18 +23,30 @@ export const ModalWrap = styled.div`
     .modal-content {
         height: 50%;
         width: 80%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
         background-color: ${props => props.theme.colors.secondary};
         border-radius: 8px;
-    }
 
-    .modal-img {
-        height: 50px;
-        width: 50px;
+        &__wrap {
+            height: 80%;
+            width: 80%;
+            border-radius: 10px;
+            overflow: hidden;
+            background-color: ${props => props.theme.colors.secondaryAlt};
+        }
+    
+        &__image {
+            height: 50px;
+            width: 50px;
 
-        img {
-            height: 100%;
-            width: 100%;
-            object-fit: cover;
+            img {
+                height: 100%;
+                width: 100%;
+                object-fit: cover;
+            }
         }
     }
 `;
