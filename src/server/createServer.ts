703f65d1d64802dee29f190f1f5b90d5635a7f52
EvertonMirand <everton.mirandav@gmail.com>
2022-02-14 17:05:35 -0300

    Altera server

--- src/server/createServer.ts
+++ src/server/createServer.ts
@@ -6,12 +6,15 @@ const createFeedServer = () => {
     {
       id: 1,
       name: 'Everton',
+      photoUrl: 'http://www.rachegebran.com.br/wp-content/uploads/perfil.jpg',
       message:
         'Bacon ipsum dolor amet doner pancetta boudin, burgdoggen venison tri-tip shank. Jerky chicken chislic, buffalo landjaeger pork belly ball tip pork tail corned beef leberkas bresaola tenderloin pork chop. Pastrami turkey capicola bacon chicken ball tip. Jerky meatloaf biltong andouille. Strip steak t-bone doner pork, swine tail pork loin jowl.',
     },
     {
       id: 2,
       name: 'Alguém',
+      photoUrl:
+        'https://conteudo.imguol.com.br/blogs/174/files/2018/05/iStock-648229868-1024x909.jpg',
       message:
         'Burgdoggen salami leberkas sirloin kevin cow shank ham tail porchetta jowl tri-tip strip steak turkey. Buffalo pork chop tenderloin, landjaeger cow porchetta venison flank doner salami pig rump tri-tip.',
     },
