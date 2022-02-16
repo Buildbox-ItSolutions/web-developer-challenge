eff8d15552b250af32ad5ced0e7c59e64d214689
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-16 16:07:51 -0300

    feat: :sparkles: Passing callback to DiscardButton

--- src/components/DiscardButton/index.tsx
+++ src/components/DiscardButton/index.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import defaultTheme from '../../theme/defaultTheme'
 import * as S from './style'
 
-const DiscardButton: React.FC = () => {
-  return <S.Wrapper theme={defaultTheme}>Descartar</S.Wrapper>
+interface DiscardButtonProps {
+  discardPostClick: () => void
+}
+
+const DiscardButton: React.FC<DiscardButtonProps> = ({ discardPostClick }) => {
+  return (
+    <S.Wrapper onClick={discardPostClick} theme={defaultTheme}>
+      Descartar
+    </S.Wrapper>
+  )
 }
 
 export default DiscardButton
