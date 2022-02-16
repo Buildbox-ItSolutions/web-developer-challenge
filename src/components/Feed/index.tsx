0d2b2061c5364f676a6acffcaeb6027affd2409e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-16 16:09:25 -0300

    feat: :sparkles: Adding post deletion logic in the Feed component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import * as S from './style'
+import React, { useMemo } from 'react'
+import { UseMyPostContext } from '../../context/PostDataContext/useMyPostContext'
+import { v4 as uuidv4 } from 'uuid'
 import Post from '../Post'
+import * as S from './style'
 
-const dataExample = [
-  {
-    id: 1,
-    photo: '/image/image.png',
-    title: 'Lorem ipsum dolor sit amet',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.',
-    author: 'Manuela Oliveira'
-  },
-  {
-    id: 2,
-    photo: '/image/image.png',
-    title: 'Lorem ipsum dolor sit amet',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.',
-    author: 'Manuela Oliveira'
-  }
-]
+type PostType = { id: number; name: string; photo: string; message: string }
+type EventClickButtonDeletePost = React.BaseSyntheticEvent<HTMLButtonElement>
 
 const Feed: React.FC = () => {
+  const [postState, setPostState] = UseMyPostContext()
+
+  const newPostState = useMemo(() => {
+    return postState.map((post: PostType, index: number) => {
+      return {
+        ...post,
+        id: index
+      }
+    })
+  }, [postState])
+
+  const handleDeletePostClick = (event: EventClickButtonDeletePost) => {
+    const postIndex = event.currentTarget.parentElement.getAttribute('id')
+    newPostState.splice(postIndex, 1)
+    setPostState([...newPostState])
+  }
+
   return (
     <S.Wrapper>
-      {dataExample.map(({ id, photo, title, body, author }) => (
-        <Post
-          key={id}
-          photo={photo}
-          title={title}
-          body={body}
-          author={author}
-        />
-      ))}
+      {newPostState &&
+        newPostState.map((post: PostType) => (
+          <Post
+            id={post.id}
+            key={uuidv4()}
+            photo={post.photo}
+            message={post.message}
+            name={post.name}
+            deletePostButton={handleDeletePostClick}
+          />
+        ))}
     </S.Wrapper>
   )
 }
