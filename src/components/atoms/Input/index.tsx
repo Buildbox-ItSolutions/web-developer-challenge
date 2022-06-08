452aa96590521e4c934c148b595a265a311d32fd
Isaac Lopes <isaaclopinho@gmail.com>
2022-06-08 02:44:28 -0300

    exporting html input type

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { ThemeSpacing } from 'styles/theme';
 
-type HTMLInputType = React.DetailedHTMLProps<
+export type HTMLInputType = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
