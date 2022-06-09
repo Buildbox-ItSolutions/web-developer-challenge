973875fca0cb3c8886b7f68b3d33beaf165f02cd
Mateus Jardim <dteteus@gmail.com>
2022-06-09 00:48:22 -0300

    Desafio

---
+++ src/components/Post/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+export const PostText = styled.span`
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    text-align: left;
+    color: #9f9f9f;
+`
+
+export const PostLabel = styled.span`
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.33;
+    letter-spacing: 0.29px;
+    text-align: left;
+    color: #5f5f5f;
+`
+
+export const PostName = styled.span`
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: left;
+    color: #7a7a7a;
+`
+
+export const PostDelIcon = styled.img`
+    width: 20px;
+    height: 20px;
+    padding: 0;
+    object-fit: contain;
+    cursor: pointer;
+`
