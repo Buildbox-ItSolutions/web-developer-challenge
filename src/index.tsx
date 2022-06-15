ea43d84cd53c750d2801e815818b30a6ac921dc9
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 15:33:49 -0300

    perf: removing unnecessary files and organizing folders.

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 reportWebVitals();
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Routes = () => {
+import { Route, Switch } from "react-router-dom";
 
-    return (
-        <>
-        </>
-    );
-}
+import Home from "../pages/Home";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
+};
 
 export default Routes;
