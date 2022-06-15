c2c04cbd0a3f2bc85733a937c1ba6928a89f47dc
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 15:49:19 -0300

    fix: import GlobalStyle on index

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
+import { GlobalStyle } from './styles/GlobalStyle';
+
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -11,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+    <GlobalStyle />
       <App />
     </React.StrictMode>
   </BrowserRouter>
