fe6b5d43fa1a11289600a65b0e2a2fe3397ff445
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 15:58:13 -0300

    feat: base of the form created.

--- src/components/CreateAPost/index.tsx
+++ src/components/CreateAPost/index.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
 
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { useForm } from 'react-hook-form';
+import schema from '../../schemas';
+import { Button, Container, ImageInput, InputArea } from './styled';
+
+
+interface Data {
+    name: string,
+    message: string
+}
+
 const CreateAPost = () => {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<Data>({
+        resolver: yupResolver(schema)
+    });
+
+    const onPublish = (data: Data) => {
+
+    }
 
     return (
         <>
-        CreateAPost
+            <Container onSubmit={handleSubmit(onPublish)}>
+
+                <ImageInput>
+
+                </ImageInput>
+
+                <InputArea>
+                    <input
+                        type="text"
+                        placeholder="Digite seu nome"
+                        {...register("name")}
+                    />
+                    <textarea
+                        cols={30}
+                        rows={10}
+                        placeholder="Mensagem"
+                        {...register("message")}
+                    ></textarea>
+                    <div>
+                        <Button type="button" >Descartar</Button>
+                        <Button id="publish" publish={true} type="submit">Publicar</Button>
+                    </div>
+                </InputArea>
+
+            </Container>
+
         </>
     );
 }
