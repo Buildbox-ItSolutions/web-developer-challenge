1245958ca08dae1df042072cd93374418c0d28d8
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 15:49:46 -0300

    feat: header styling with var().

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import { Container, FeedSpan, Header } from './styled';
+import Logo from "../../assets/images/bx-logo.png";
+
+import CreateAPost from '../../components/CreateAPost';
+import Feed from '../../components/Feed';
+
 const Home = () => {
 
     return (
         <>
-            Home
+            <Header>
+                <img src={Logo} alt="Logo buildbox" />
+            </Header>
+
+            <Container>
+                <CreateAPost />
+
+                <FeedSpan>
+                    Feed
+                </FeedSpan>
+
+                <Feed />
+
+            </Container>
         </>
     );
 }
