0e3b7ff0dc26adedf564db5d8bb6396acb7de48a
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 16:15:36 -0300

    feat: form working with UX(toastify).

--- src/components/CreateAPost/index.tsx
+++ src/components/CreateAPost/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Button, Container, ImageInput, InputArea } from './styled';
 import NoImage from "../../assets/images/no-image.png";
 import Trash from "../../assets/images/trash.png";
 
 import { yupResolver } from '@hookform/resolvers/yup';
+
+import { toast } from "react-toastify";
+import { Button, Container, ImageInput, InputArea } from './styled';
 import { useForm } from 'react-hook-form';
 import schema from '../../schemas';
-
-
 import { useData } from '../../providers/Data';
 
 interface Data {
@@ -18,7 +18,7 @@ interface Data {
 
 const CreateAPost = () => {
 
-    const { profileImage, setProfileImage, showTrash } = useData();
+    const { profileImage, setProfileImage, resetForm, showTrash, setShowTrash, publish } = useData();
 
     const handleImage = (file: any) => {
         const reader = new FileReader();
@@ -37,8 +37,45 @@ const CreateAPost = () => {
         resolver: yupResolver(schema)
     });
 
-    const onPublish = (data: Data) => {
+    useEffect(() => {
+        if (errors.name) {
+            toastMessage("error", `${errors.name.message}`);
+        } else if (errors.message) {
+            toastMessage("error", `${errors.message.message}`);
+        }
+    }, [errors])
 
+    const toastMessage = (type: string, message: string) => {
+        if (type === "success") {
+            toast.success(message)
+        } else if (type === "error") {
+            toast.error(message)
+        }
+    }
+
+    const onPublish = (data: Data) => {
+        const { name, message } = data;
+
+        const randomID = () => {
+            return "bbox" + Math.floor(Date.now() * Math.random()).toString(36);
+        }
+
+        const post = {
+            name: name,
+            message: message,
+            profileImage: profileImage,
+            id: randomID()
+        }
+
+        publish(post);
+        toastMessage("success", "Mensagem enviada!")
+
+        reset();
+        setProfileImage(NoImage);
+        setShowTrash(false);
+
+        const image = document.querySelector("#userImage") as HTMLScriptElement
+        image.classList.remove("updatedImage");
     }
 
     return (
@@ -78,7 +115,7 @@ const CreateAPost = () => {
                         {...register("message")}
                     ></textarea>
                     <div>
-                        <Button type="button" >Descartar</Button>
+                        <Button type="button" onClick={() => resetForm()}>Descartar</Button>
                         <Button id="publish" publish={true} type="submit">Publicar</Button>
                     </div>
                 </InputArea>
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
 
+import Providers from './providers';
+
 import { BrowserRouter } from 'react-router-dom';
 
-import Providers from './providers';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,6 +20,17 @@ root.render(
     <BrowserRouter>
       <React.StrictMode>
         <GlobalStyle />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+        />
         <App />
       </React.StrictMode>
     </BrowserRouter>
