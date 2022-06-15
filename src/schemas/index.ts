b124ea179be68ae9c817a49e2221443d28b634ca
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-06-15 15:50:37 -0300

    feat: form schemas created.

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,1 +1,13 @@
-export {}
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+    name: yup
+        .string()
+        .required("Nome necessário!")
+        .min(3, "Mínimo 3 caracteres."),
+    message: yup
+        .string()
+        .required("Mensagem necessário!"),
+});
+
+export default schema;
