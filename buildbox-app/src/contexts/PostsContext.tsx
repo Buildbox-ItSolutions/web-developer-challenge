0f5af269a9327195204088850c4ac593804e768e
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-17 23:13:37 -0300

    Convert PostsContext.js to .tsx

--- buildbox-app/src/contexts/PostsContext.tsx
+++ buildbox-app/src/contexts/PostsContext.tsx
@@ -1,20 +1,23 @@
-import { useReducer, useContext, createContext } from 'react'
+import React, { useReducer, useContext, createContext } from 'react'
+import { PostData } from '../@types'
 
-const PostsStateContext = createContext()
-const PostsDispatchContext = createContext()
+const PostsStateContext = createContext([])
+const PostsDispatchContext = createContext({} as any)
 
-const reducer = (state, action) => {
+const reducer = (state: any, action: any) => {
   switch (action.type) {
     case 'CREATE_POST':
       return [...state, action.payload]
     case 'DELETE_POST':
-      return state.filter((post) => state.indexOf(post) !== action.index)
+      return state.filter(
+        (post: PostData) => state.indexOf(post) !== action.index
+      )
     default:
       throw new Error(`Unknown action: ${action.type}`)
   }
 }
 
-export const PostsProvider = ({ children }) => {
+export const PostsProvider = ({ children }: any) => {
   const [state, dispatch] = useReducer(reducer, [])
   return (
     <PostsDispatchContext.Provider value={dispatch}>
