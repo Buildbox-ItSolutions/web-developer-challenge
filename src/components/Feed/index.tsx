1e3d9d777f9220a3bd0ff3bc514cc945cf3be90e
caio-gac <caio.correia@aluno.unc.br>
2022-08-09 18:21:29 -0300

    create post

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -2,12 +2,21 @@ import Posts from "components/Posts";
 import styled  from "styled-components";
 
 const Content = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 516px;
+`;
+const Text = styled.h1`
+    color:white;
+    margin-left:-35vw;
+    margin-top:56px;
 `;
 
 export default function Feed() {
     return (
         <Content>
+            <Text>Feed </Text>
             <Posts />
         </Content>
     );
--- src/components/PostMake/index.tsx
+++ src/components/PostMake/index.tsx
@@ -1,19 +1,72 @@
 import styled  from "styled-components";
-
+import image from 'assets/image.svg'
 const Content = styled.div`
+    display:flex;
+    flex-direction:column;
     width: 516px;
     height: 353px;
+
     margin: 40px 425px 56px;
     padding: 24px;
+
     border-radius: 3px;
     border: solid 1px #3b3b3b;
     background-color: #313131;
+`;
+const ImgPost = styled.img`
+    width: 88px;
+    height: 88px;
+    
+    margin: 0 8px 16px 190px;
+    padding: 32px;
+    object-fit: contain;
+    
+    border-radius: 36px;
+    border: solid 1px #4b4b4b;
+    background-color: rgba(75, 75, 75, 0);
+`;
+const NameInput = styled.input`
+    width: 468px;
+    height: 40px;
+    margin: 16px 0 8px;
+    padding: 12px 351px 11px 16px;
+    border-radius: 8px;
+    border:none;
+    background-color: #494949;
+    outline:none;
+    color:white;
+    ::placeholder{
+        width: 101px;
+        height: 17px;
+        font-family: Roboto, sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.29;
+        letter-spacing: normal;
+        text-align: left;
+        color: #9f9f9f;
+    }
+`;
+const AreaInput = styled.input`
+    width: 468px;
+    height: 80px;
+    margin: 8px 0 32px;
+    padding: 12px 382px 51px 16px;
+    border-radius: 8px;
+    border:none;
+    background-color: #494949;
+    outline:none;
+    color:white;
 `;
 
 export default function PostMake() {
     return (
         <Content>
-            
+            <ImgPost src={image} />
+            <NameInput placeholder='Digite seu nome'/>
+            <AreaInput placeholder='Mensagem'/>
         </Content>
     );
 }
