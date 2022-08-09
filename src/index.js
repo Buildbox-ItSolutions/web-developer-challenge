218821893189b805c26fdbcc19db392a3227bb2f
higorasilverio <higorasilverio@outlook.com>
2022-08-09 09:14:00 -0300

    (general): improve folders and build configs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { createRoot } from "react-dom/client";
-const container = document.querySelector("#root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
