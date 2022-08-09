2e0ab46bcc266a7e1ba4b0092782437389766578
Claudio Ikeda <claudiosikeda@gmail.com>
2022-08-09 18:36:04 -0300

    chore: instalação e configuração suite de testes

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type {Config} from '@jest/types';
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "<rootDir>/setupTests.ts",
+  ],
+};
+export default config;
