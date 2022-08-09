b3fae9308a78f62cc8ecb1f3cdba373bef7f2d5c
caio-gac <caio.correia@aluno.unc.br>
2022-08-09 18:35:36 -0300

    route fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Main from 'pages/Main';
 import NotFound from 'pages/NotFound';
 export default function AppRouter(){
