10231162b1dae41251b1ada823c74b459a2d6a27
Gustavo Maia <gustavojv_maia@outlook.com>
2022-08-09 22:54:42 -0300

    Dados
    Nome: Gustavo Maia;
    e-mail: gustavojv_maia@outlook.com ; telefone: +55 48 9 9935-3180

---
+++ buildbox/src/components/NewComment.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NewComent = () => {
+    return(
+<form>
+      <input type="text" class="todo-input" />
+      <button class="todo-button" type="submit">
+        <i class="fas fa-plus-square"></i>
+      </button>
+      <div class="select">
+        <select name="todos" class="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    );
+};
+
+export default NewComent;
