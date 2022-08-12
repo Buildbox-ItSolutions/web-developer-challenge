9837337f829b93c1c58417675676fc808f00921f
Will <willian.ralf.rn@gmail.com>
2022-08-11 23:13:58 -0300

    style: border in select image - create post component

--- src/components/CreatePost/style.ts
+++ src/components/CreatePost/style.ts
@@ -8,7 +8,7 @@ export const CreatePostContainer = styled.div`
   width: 100%;
   min-height: 353px;
 
-  .card{
+  .card {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -20,14 +20,14 @@ export const CreatePostContainer = styled.div`
     border: 1px solid var(--border);
     border-radius: 3px;
 
-    form{
+    form {
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
       width: 100%;
 
-      .selectImage{
+      .selectImage {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -38,117 +38,115 @@ export const CreatePostContainer = styled.div`
         border-radius: 36px;
         cursor: pointer;
 
-        transition: .4s;
+        transition: 0.4s;
 
-        :hover{
-          border: 1px solid var(--white);
-
+        :hover {
+          border: 1px solid var(--gray-200);
         }
-  
-        input{
+
+        input {
           display: none;
         }
       }
-  
-      .inputName{
+
+      .inputName {
         width: 100%;
         padding: 12px;
         margin-bottom: 8px;
         background-color: var(--gray-300);
-        border:none;
+        border: none;
         border-radius: 8px;
         color: var(--white);
         font-size: 14px;
 
-        transition: .4s;
+        transition: 0.4s;
 
-        :focus{
+        :focus {
           background-color: var(--gray-200);
-
         }
 
-
-        ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        ::placeholder {
+          /* Chrome, Firefox, Opera, Safari 10.1+ */
           color: var(--gray-50);
           opacity: 1; /* Firefox */
         }
 
-        :-ms-input-placeholder { /* Internet Explorer 10-11 */
-        color: var(--gray-50);
+        :-ms-input-placeholder {
+          /* Internet Explorer 10-11 */
+          color: var(--gray-50);
         }
 
-        ::-ms-input-placeholder { /* Microsoft Edge */
-        color: var(--gray-50);
+        ::-ms-input-placeholder {
+          /* Microsoft Edge */
+          color: var(--gray-50);
         }
-
       }
 
-      textarea{
+      textarea {
         max-width: 100%;
         min-width: 100%;
         min-height: 80px;
         padding: 12px;
         background-color: var(--gray-300);
-        border:none;
+        border: none;
         border-radius: 8px;
         color: var(--white);
         font-size: 14px;
 
-        transition: .4s;
+        transition: 0.4s;
 
-        :focus{
+        :focus {
           background-color: var(--gray-200);
-
         }
 
-        ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        ::placeholder {
+          /* Chrome, Firefox, Opera, Safari 10.1+ */
           color: var(--gray-50);
           opacity: 1; /* Firefox */
         }
 
-        :-ms-input-placeholder { /* Internet Explorer 10-11 */
-        color: var(--gray-50);
+        :-ms-input-placeholder {
+          /* Internet Explorer 10-11 */
+          color: var(--gray-50);
         }
 
-        ::-ms-input-placeholder { /* Microsoft Edge */
-        color: var(--gray-50);
+        ::-ms-input-placeholder {
+          /* Microsoft Edge */
+          color: var(--gray-50);
         }
       }
 
-      .buttonsArea{
+      .buttonsArea {
         display: flex;
         justify-content: flex-end;
         width: 100%;
         margin-top: 32px;
 
-        .descartar{
+        .descartar {
           padding: 12px 24px;
           background-color: transparent;
           color: var(--gray-200);
           text-decoration: underline;
 
-          transition: .4s;
+          transition: 0.4s;
 
-          :hover{
+          :hover {
             color: var(--white);
-
           }
         }
 
-        .publicar{
+        .publicar {
           padding: 12px 24px;
           background-color: var(--gray-200);
           color: var(--background);
           border-radius: 8px;
 
-          transition: .4s;
+          transition: 0.4s;
 
-          :hover{
+          :hover {
             background-color: var(--green);
             color: var(--white);
-
           }
-
         }
       }
     }
