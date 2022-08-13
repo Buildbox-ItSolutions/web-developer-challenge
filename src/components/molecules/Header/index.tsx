983380504eedb6b9371170e62afaab76d9d0746c
Renan.Olovics <renan.olovics@gmail.com>
2022-08-13 01:25:53 -0300

    fix: Header icon

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -5,7 +5,7 @@ import * as S from './styles'
 export const Header = () => {
   return (
     <S.Container>
-      <Icon />
+      <Icon icon='IcLogo' width='100px' height='45px' />
     </S.Container>
   )
 }
