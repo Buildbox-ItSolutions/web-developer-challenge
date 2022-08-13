31561b3a5faa6d56551f1d792b64a4cd9f6028d0
Renan.Olovics <renan.olovics@gmail.com>
2022-08-12 23:47:45 -0300

    feat: global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { AppProvider } from '@/contexts'
+import { GlobalStyles } from '@/styles'
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AppProvider>
       <App />
+      <GlobalStyles />
     </AppProvider>
   </React.StrictMode>
 )
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,7 @@
 import { colors, shapes, typography } from './tokens'
 
+export * from './global'
+
 export const theme = {
   colors,
   shapes,
