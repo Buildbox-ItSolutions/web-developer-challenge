564912717cc0f8cb39eb97068693067fcd73577a
Renan.Olovics <renan.olovics@gmail.com>
2022-08-13 01:04:36 -0300

    feat: vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths(), svgr()]
 })
