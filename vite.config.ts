d91c08531984654271b0ebb71cef8429cec39fb1
Renan.Olovics <renan.olovics@gmail.com>
2022-08-14 01:04:21 -0300

    chore: plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()]
+  plugins: [react(), tsconfigPaths(), svgr(), checker({ typescript: true })]
 })
