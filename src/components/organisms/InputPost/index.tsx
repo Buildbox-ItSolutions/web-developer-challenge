eea7c7284774ebf0d0825e28efdd43f1cc01f09d
Renan.Olovics <renan.olovics@gmail.com>
2022-08-14 03:27:36 -0300

    refactor: inputPost integrated

--- src/components/organisms/InputPost/index.tsx
+++ src/components/organisms/InputPost/index.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 
 import { Input, TextArea } from '@/components/molecules'
 import { Avatar, Button } from '@/components/atoms'
-
+import { createPost } from '@/services'
 import * as S from './styles'
 
 type FormData = {
@@ -16,12 +16,14 @@ export const InputPost = () => {
 
   const picFile = watch('picture')
   const pic = picFile ? URL.createObjectURL(picFile[0]) : ''
+  const resetForm = () => reset({ name: '', content: '', picture: undefined })
 
-  const handleSubmitForm = (data: FormData) => {
-    console.log(data)
-  }
-  const cleanForm = () => {
-    reset({ name: '', content: '', picture: undefined })
+  const handleSubmitForm = async (data: FormData) => {
+    await createPost({
+      ...data,
+      picture: URL.createObjectURL(data.picture[0])
+    })
+    resetForm()
   }
 
   return (
@@ -29,9 +31,8 @@ export const InputPost = () => {
       <Avatar pic={pic} {...register('picture', { required: true })} />
       <Input fullWidth placeholder='Digite seu nome' {...register('name', { required: true })} />
       <TextArea fullWidth placeholder='Mensagem' {...register('content', { required: true })} />
-
       <S.Buttons>
-        <Button variant='empty' onClick={cleanForm}>
+        <Button variant='empty' onClick={resetForm}>
           Descartar
         </Button>
         <Button variant='full' onClick={handleSubmit(handleSubmitForm)}>
