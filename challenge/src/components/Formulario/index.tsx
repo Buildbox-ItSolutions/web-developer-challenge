3f73c6ad30d77a0c8bbaddc67b79b48534e75f88
Elias de Souza <elias99-souza@hotmail.com>
2022-08-15 23:45:08 -0300

    challange

---
+++ challenge/src/components/Formulario/index.tsx
@@ -0,0 +1,23 @@
+
+export default function Formulario () {
+    return(
+        <div >
+            <form >
+              <div>
+                  <input type="text" value={"Elias de Souza"} />
+              </div>
+
+              <div>
+                 <input type="text" value={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, autem error nostrum incidunt veniam temporibus explicabo dolores non, voluptatum deserunt adipisci harum? Natus aspernatur, ea atque ipsum libero porro ad?'} />
+              </div>
+                <div>
+                    <button>Descartar</button>
+                    <button>Publicar</button>
+                </div>
+            </form>
+          
+
+        </div>
+
+    )
+}
