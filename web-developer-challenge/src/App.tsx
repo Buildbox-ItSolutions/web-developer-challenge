b72e32c52d79564e9627ee944a3cf08aab47208e
leandrochs <leandrochagask@gmail.com>
2022-08-16 00:56:04 -0300

    feat: adiciona secoes da pagina foi adicionado Header UserInterface e Fedd

--- web-developer-challenge/src/App.tsx
+++ web-developer-challenge/src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Feed, Header, UserInterface } from './sections';
+import * as S from './styles';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite plus React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <S.Main>
+      <Header />
+      <UserInterface />
+      <Feed />
+    </S.Main>
+  );
 }
 
-export default App
+export default App;
