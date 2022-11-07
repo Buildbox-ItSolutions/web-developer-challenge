ce889c6e81fb2a19ba4705ab08572e13ac2e0307
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-11-07 20:57:59 -0300

    test: first test

--- cypress/e2e/project.cy.ts
+++ cypress/e2e/project.cy.ts
@@ -3,4 +3,8 @@
 
 describe("InfoBox Test", () => {
   before(() => cy.visit("http://localhost:3000/"));
+
+  it("Test type in input name", () => {
+    cy.get('[data-cy="input-name"]').type("test name");
+  });
 });
