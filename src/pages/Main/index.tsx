369bd46e521615b0e5637f6074c0d00d55c0069e
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-11-08 13:24:26 -0300

    chore: add feed component in main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,4 @@
+import { Feed } from '@components/Feed';
 import { Form } from '@components/Form';
 
 import * as S from './styles';
@@ -6,6 +7,7 @@ export default function Main() {
   return (
     <S.Container>
       <Form />
+      <Feed />
     </S.Container>
   );
 }
