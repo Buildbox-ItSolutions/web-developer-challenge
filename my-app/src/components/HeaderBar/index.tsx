1b7a3e11ec9a821f89130a2a39b11f0cb458ee5f
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2022-11-08 08:25:30 -0300

    component header bar

---
+++ my-app/src/components/HeaderBar/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+import Col from 'react-bootstrap/esm/Col'
+import Row from 'react-bootstrap/esm/Row'
+import logo from '../../../public/bx-logo.png';
+import Image from 'next/image';
+
+export default function HeaderBar(){
+    return(
+        <Row>
+            <Col className={styles.header}>
+                <Image src={logo} alt="bx-logo.png"/>
+            </Col>
+        </Row>
+    )
+}
