19e45a9642359c06fedd999f8eb1e9170a358a7d
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-11-08 12:35:18 -0300

    chore: new image to base64 function

---
+++ src/utils/image-to-base64.ts
@@ -0,0 +1,9 @@
+export const imageToBase64 = (
+  file: File,
+): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
