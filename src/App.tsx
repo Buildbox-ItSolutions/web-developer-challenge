0533e3b2cdeda5365b94d0d1ec58b2146792aad5
shermondevelopment <victorshermon@yahoo.com>
2023-02-04 08:26:35 -0300

    chore: including library prettier in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">My app challange</div>
 }
 
-export default App;
+export default App
