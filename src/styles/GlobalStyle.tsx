bab154976ea09c4046fb3b0a2236667abf10e020
shermondevelopment <victorshermon@yahoo.com>
2023-02-04 08:47:11 -0300

    feat: adding gobal style in app

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #343434;
+    color: #9f9f9f;
+    font-family: 'Roboto', sans-serif;
+  }
+`
+export default GlobalStyle
