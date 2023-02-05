a00d52cf4871a921555123cd61091fc5f418d5e6
nathan3boy <nathan3boss@gmail.com>
2023-02-05 17:25:28 -0300

    feat(core): created textarea component

---
+++ apps/web/src/components/create-post/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+import { CreatePostProps } from './model';
+import { CreatePostStyled } from './styles';
+
+export const CreatePost: FC<CreatePostProps> = () => {
+  return <CreatePostStyled></CreatePostStyled>;
+};
