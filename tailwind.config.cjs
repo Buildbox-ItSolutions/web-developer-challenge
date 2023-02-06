4ebe03f306999cd0d6d67b782a1a529a188d6678
Gabriel Santana Oliveira de Paula <gabsop@hotmail.com>
2023-02-06 00:16:44 -0300

    Beggining of layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#343434",
+          30: "#9f9f9f",
+          40: "#808080",
+          50: "#5E5E5E",
+          60: "#494949",
+          70: "#3b3b3b",
+          80: "#313131",
+          90: "#2b2b2b",
+        },
+        green: "#70BB00",
+      },
+    },
   },
   plugins: [],
 };
