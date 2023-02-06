d1d966d40e5a5c13f9056e9415e2319813caa6b0
shermondevelopment <victorshermon@yahoo.com>
2023-02-06 20:30:36 -0300

    feat: creating component of input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/* style */
+import * as S from './style'
+
+const Input = (): React.ReactElement => {
+  return <S.Input />
+}
+
+export default Input
---
+++ src/components/Input/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  width: 100%;
+  border: none;
+  color: #9f9f9f;
+  outline: 0;
+  background: #494949;
+`
