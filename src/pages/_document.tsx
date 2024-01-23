78f1188c32fe341436953dc011571b660be9e526
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2024-01-23 13:57:01 -0300

    chore: first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
