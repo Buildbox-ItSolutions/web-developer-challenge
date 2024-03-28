4b25da89f76d9e99afeac74119de6ec093a62b7a
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-03-28 15:03:14 -0300

    add styles

--- src/pages/Feed/style.ts
+++ src/pages/Feed/style.ts
@@ -2,7 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 90%;
-  height: 353px;
+  height: 413px;
+  max-width: 516px;
+  margin: 40px auto 56px;
+  padding: 2%;
+  border-radius: 3px;
+  border: solid 1px #3b3b3b;
+  background-color: #313131;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("/photo-upload.png");
+  width: 80px;
+  height: 80px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`;
+
+export const Avatar = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
+
+export const FeedItemContainer = styled.div`
   max-width: 516px;
   margin: 40px auto 56px;
   padding: 2%;
@@ -30,4 +57,43 @@ export const Message = styled.input`
   color: #fff;
 `;
 
-export const Button = styled.button``;
+export const MessageBox = styled.div`
+  word-wrap: break-word;
+  display: flex;
+  color: #9f9f9f;
+`;
+
+export const ImageInput = styled.input`
+  display: none;
+`;
+
+export const Button = styled.button`
+  margin-top: 60px;
+  background-color: #5f5f5f;
+  border-radius: 4px;
+  padding: 10px 24px;
+  float: right;
+  &:hover,
+  &:active {
+    background-color: #71bb00;
+  }
+`;
+
+export const DeleteButton = styled.div`
+  float: right;
+  cursor: pointer;
+`;
+
+export const ClearButton = styled.button`
+  margin-top: 10px;
+  margin: 20px;
+  border: none;
+  color: #5f5f5f;
+  cursor: pointer;
+  margin-top: 70px;
+  float: right;
+`;
+
+export const Info = styled.div`
+  color: #9f9f9f;
+`;
