38a259f0ee055339185b04539602afec2e7971bb
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-03-27 22:38:16 -0300

    feat: add Header

---
+++ src/pages/Header/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  background-color: #2b2b2b;
+  height: 93px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
