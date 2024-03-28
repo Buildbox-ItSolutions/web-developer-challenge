c58240a3d16900012205a6e4806a486d90127855
Raphael Borba <raphaelbsx@gmail.com>
2024-03-28 13:49:38 -0300

    feat: finish form to create post

--- src/components/CreatePost/index.tsx
+++ src/components/CreatePost/index.tsx
@@ -1,20 +1,65 @@
+import { useState } from "react";
 import Card from "../Card";
-import { ContainerButtonS, ContainerInputS, InputS, TextareaS } from "./style";
+import { ContainerButtonS, ContainerInputS, CreateButtonS, FileInputS, InputS, LabelFileS, ResetButtonS, SelectImageS, TextareaS, TrashImgS } from "./style";
 
 
 export default function CreatePost() {
 
+    const [imagePath, setImagePath] = useState('')
+    const [name, setName] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [canpost, setCanpost] = useState<boolean>(false)
+
+    function handleChange(event: any) {
+        console.log(event)
+        if (!event.target.files[0]) setImagePath("")
+        else setImagePath(URL.createObjectURL(event.target.files[0]))
+        verifyIfCanPost()
+    }
+
+    function verifyIfCanPost() {
+        console.log(name, message, imagePath)
+        if (name.length > 2 && message.length > 4 && imagePath.length > 5) {
+            setCanpost(true)
+        } else {
+            setCanpost(false)
+        }
+    }
+
+    function deleteImage(){
+        setCanpost(false)
+        setImagePath("")
+        setName("")
+        setMessage("")
+    }
+
     return (
         <Card>
-            <form >
+            <form onChange={verifyIfCanPost}>
+                <div style={{display:"flex", flexDirection:"row", justifyContent:"center" }}>
+                    <label htmlFor="fileInput">
+                        <LabelFileS>
+                            {
+                                imagePath ?
+                                    <SelectImageS src={imagePath} alt="" />
+                                    :
+                                    <SelectImageS src="/image.svg" alt="" />
+                            }
+
+                        </LabelFileS>
+                    </label>
+                    {imagePath && <TrashImgS src="/trash.svg" alt="" onClick={deleteImage} />}
+                    <FileInputS onChange={handleChange} id="fileInput" type="file" accept="image/png, image/jpeg" />
+                </div>
+
                 <ContainerInputS>
-                    <InputS type="text" placeholder="Digite seu nome" required />
-                    <TextareaS placeholder="Mensagem"  required/>
+                    <InputS type="text" placeholder="Digite seu nome" required onChange={(e) => setName(e.target.value)} />
+                    <TextareaS placeholder="Mensagem" required onChange={(e) => setMessage(e.target.value)} />
                 </ContainerInputS>
 
                 <ContainerButtonS>
-                    <button type="reset">Descartar</button>
-                    <button type="submit">Publicar</button>
+                    <ResetButtonS type="reset" onClick={()=>setCanpost(false)}>Descartar</ResetButtonS>
+                    <CreateButtonS canpost={canpost} disabled type="submit">Publicar</CreateButtonS>
                 </ContainerButtonS>
             </form>
         </Card>
--- src/components/CreatePost/style.ts
+++ src/components/CreatePost/style.ts
@@ -1,36 +1,51 @@
 import styled from "styled-components";
 
 
-export const InputS = styled.input`
-
-    width: 100%;
-    background-color: #494949;
-    color: white;
-    border: none;
-    outline: none;
-    height: 40px;
-    border-radius: 8px;
-    padding: 16px 12px;
+export const LabelFileS = styled.div`
+    
+    width: 88px;
+    height: 88px;
+    border: #4b4b4b 1px solid;
+    border-radius: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-inline: auto;
+    overflow: hidden;
+`
+export const FileInputS = styled.input`
+    display: none;
 `
 
-export const TextareaS = styled.textarea`
-
-    width: 100%;
-    background-color: #494949;
-    color: white;
-    border: none;
-    outline: none;
-    border-radius: 8px;
-    padding: 16px 12px;
-    resize: vertical;
-    min-height: 80px;
+export const SelectImageS = styled.img`
+    max-width: 100%;
+    max-height: 100%;
 `
 
 export const ContainerInputS = styled.div`
-
     display: flex;
     flex-direction: column;
     gap: 8px;
+    margin-top: 16px;
+
+    input, textarea {
+        width: 100%;
+        background-color: #494949;
+        color: white;
+        border: none;
+        outline: none;
+        border-radius: 8px;
+        padding: 16px 12px;
+    }
+`
+
+export const InputS = styled.input`
+    height: 40px;
+`
+
+export const TextareaS = styled.textarea`
+    resize: none;
+    min-height: 80px;
 `
 
 export const ContainerButtonS = styled.div`
@@ -41,4 +56,40 @@ export const ContainerButtonS = styled.div`
     flex-direction: row;
     gap: 24px;
     justify-content: end;
+
+    button{
+        outline: none;
+        border: none;
+    }
+
+    @media screen and (max-width:300px){
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+export const CreateButtonS = styled.button<{canpost:boolean}>`
+
+    width: 98px;
+    height: 41px;
+    padding: 12px 24px;
+    border-radius: 8px;
+    background-color: ${props => props.canpost ? "#71bb00" : "#5f5f5f"};
+    color: ${props => props.canpost ? "white" : "#313131"};
+    cursor: ${props => props.canpost ? "pointer" : "not-allowed"};
+`
+
+export const ResetButtonS = styled.button`
+
+    background-color: #313131;
+    cursor: pointer;
+    color: #5f5f5f;
+    text-decoration: underline;
+`
+
+export const TrashImgS = styled.img`
+
+   position: relative;
+   left: 16px;
+    
 `
