e32325e9f32af4614d46d12c3b768eedb574b039
Fransuelton <elton6103@gmail.com>
2024-03-28 15:10:33 -0300

    refactor: Add useState and functionality to add and remove posts in Feed component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,15 +1,75 @@
-import { PostCard } from ".";
+import { useState } from "react";
+import { PostCard, PostForm } from ".";
+
 import PhotoBaseOne from "../assets/photo-base-one.png";
 import PhotoBaseTwo from "../assets/photo-base-two.png";
+import PhotoBaseThree from "../assets/photo-base-three.png";
 
 import styled from "styled-components";
 
-const Feed = () => {
+const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      name: "Manuela Oliveira",
+      message:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.",
+      image: PhotoBaseOne,
+    },
+    {
+      id: 2,
+      name: "Manuela Oliveira",
+      message:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.",
+      image: PhotoBaseTwo,
+    },
+    {
+      id: 3,
+      name: "Manuela Oliveira",
+      message:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis.",
+      image: PhotoBaseThree,
+    },
+  ]);
+
+  interface INewPost {
+    id: number;
+    name: string;
+    message: string;
+    image?: string;
+  }
+
+  const addPost = (newPost: INewPost) => {
+    const image = newPost.image
+      ? newPost.image
+      : posts.length % 3 === 0
+      ? PhotoBaseOne
+      : posts.length % 3 === 1
+      ? PhotoBaseTwo
+      : PhotoBaseThree;
+
+    const postWithImage = { ...newPost, image };
+
+    setPosts([postWithImage, ...posts]);
+  };
+
+  const removePost = (id: number) => {
+    setPosts(posts.filter((post) => post.id !== id));
+  };
   return (
     <>
+      <PostForm addPost={addPost} />
       <StyledH3>Feed</StyledH3>
-      <PostCard image={PhotoBaseOne} message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis." name="Manuela Oliveira" />
-      <PostCard image={PhotoBaseTwo} message ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus." name="Manuela Oliveira"/>
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          id={post.id}
+          image={post.image}
+          message={post.message}
+          name={post.name}
+          onRemove={removePost}
+        />
+      ))}
     </>
   );
 };
