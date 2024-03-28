62b3124dbafc05d35f97247b8854700cd5b68378
Raphael Borba <raphaelbsx@gmail.com>
2024-03-28 10:42:31 -0300

    Initial configs and add Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainPage from './pages/Main';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle/>
+    <MainPage />
   </React.StrictMode>
 );
 
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function MainPage(){
+
+    return (
+        <main>
+            as
+        </main>
+    )
+}
