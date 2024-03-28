4b2146fa2b40438536f52fd316679a6f06a3960e
Raphael Borba <raphaelbsx@gmail.com>
2024-03-28 15:23:02 -0300

    feat: add zustand in project

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,14 @@
 import CreatePost from "../../components/CreatePost";
 import Post from "../../components/Post";
+import useStore from "../../store/posts";
 import { MainS, ContainerS, H1S } from "./style";
 
 
 export default function MainPage() {
+
+    const {posts} = useStore()
+
+    console.log(posts)
 
     return (
         <MainS>
---
+++ src/store/posts.ts
@@ -0,0 +1,23 @@
+import {create} from 'zustand'
+import { Post } from '../types/post'
+
+
+interface PostsState {
+
+    posts: Post[],
+    deletePost: (index:number) =>void;
+    addPost: (post:Post) => void;
+}
+
+const useStore = create<PostsState>()((set)=>({
+    posts:[],
+    deletePost:(index) => set((state) => ({posts: state.posts.filter((post, i) => i !== index)})),
+    addPost: (post) => set((state) => {
+        const listPost = state.posts
+        listPost.push(post)
+        return { posts:listPost}
+    })
+}))
+
+export default useStore
+
