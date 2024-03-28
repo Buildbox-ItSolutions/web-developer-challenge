8e2254781ec57c329966d88d15b0cb2a327469d6
Fransuelton <elton6103@gmail.com>
2024-03-28 12:54:38 -0300

    feat: Add PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+const PostCard = (props) => {
+  return (
+    <StyledDiv>
+      <StyledDivContainerIcon>
+        <StyledA>X</StyledA>
+      </StyledDivContainerIcon>
+
+      <StyledDivContainer>
+        <div>
+          <StyledImg src={props.image} alt="" />
+        </div>
+        <StyledDivContainerText>
+          <StyledPMessage>{props.message}</StyledPMessage>
+
+          <p className="submittedby">Enviado por</p>
+          <p className="username">{props.name}</p>
+        </StyledDivContainerText>
+      </StyledDivContainer>
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div`
+  background-color: var(--rectangle-color);
+  width: 51.6rem;
+  border-radius: 0.3rem;
+  border: solid 0.1rem var(--border-color);
+  padding: 1.2rem 1.2rem 3.2rem 2.4rem;
+  margin-bottom: 1.6rem;
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
+`;
+
+const StyledDivContainerIcon = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+const StyledDivContainer = styled.div`
+  display: flex;
+
+  @media (max-width: 480px) {
+    margin-top: 2.5rem;
+  }
+`;
+
+const StyledDivContainerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 3.2rem;
+
+  .submittedby {
+    font-weight: 500;
+    font-size: 1.2rem;
+    letter-spacing: 0.029rem;
+    margin-top: 2.4rem;
+    color: var(--auxiliar-gray);
+  }
+
+  .username {
+    font-size: 1.4rem;
+    font-weight: 400;
+    color: var(--warm-gray-two);
+  }
+
+  @media (max-width: 480px) {
+    margin-left: 1.6rem;
+  }
+`;
+
+const StyledA = styled.a`
+  color: var(--icons-color);
+  border: 0.1rem solid var(--icons-color);
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  padding: 0.6rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  width: 8.8rem;
+  height: 8.8rem;
+  border-radius: 3.6rem;
+`;
+
+const StyledPMessage = styled.p`
+  font-size: 1.6rem;
+  color: var(--auxiliar-text-color);
+  text-align: left;
+  width: 34.8rem;
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
+`;
+
+export { PostCard };
