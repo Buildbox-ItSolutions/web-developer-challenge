e5a343400e452d377a3d397ad365b65c63a2a156
Fransuelton <elton6103@gmail.com>
2024-03-28 12:53:45 -0300

    feat: Add Button component with styled

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface IButton {
+  content: string;
+  className?: string;
+}
+
+const Button = ({ content = "Button", className }: IButton) => {
+  return <StyledButton className={className}>{content}</StyledButton>;
+};
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  width: 9.8rem;
+  height: 4.1rem;
+  padding: 1.2rem 2.4rem;
+  border: none;
+  border-radius: 0.8rem;
+  background-color: var(--primary-color);
+  font-size: 1.4rem;
+  font-family: "Roboto", sans-serif;
+  color: var(--white-color);
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: var(--button-hover-color);
+  }
+`;
+
+export { Button };
