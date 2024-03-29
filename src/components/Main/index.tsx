8446d215a1f2bde94fd5b9f38c6cc5a4c0a56a99
wallace-lw <wallacelw22@gmail.com>
2024-03-29 01:12:45 -0300

    feat: add the header to the main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from "../Header"
 import { Post } from "../Post"
 import { Wrapper } from "./style"
 
 export const Main = () => {
   return (
     <Wrapper>
+      <Header />
       <Post />
     </Wrapper>
   )
