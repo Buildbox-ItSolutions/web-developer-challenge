11d4a2c02678d506984e821f97a06d21a6c3c9ab
igoramf <igoraaf1@gmail.com>
2024-03-29 14:12:06 -0300

    style: sticky header

--- web-developer-challenge/src/components/Header/HeaderStyles.ts
+++ web-developer-challenge/src/components/Header/HeaderStyles.ts
@@ -2,6 +2,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    position: fixed; /* torna o componente fixo */
+    top: 0; /* fixa o componente no topo */
+    left: 0; /* fixa o componente à esquerda */
     display: flex;
     align-items: center;
     justify-content: center;
@@ -9,4 +12,4 @@ export const Container = styled.div`
     width: 100%;
     height: 70px;
     background-color: #2B2B2B;
-`
+`;
