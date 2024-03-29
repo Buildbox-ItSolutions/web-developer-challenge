4c0ac532df6b52767f3fb95e1f1644c8f36ac4d6
Igor Oliveira <dev.igoroliveiraa@gmail.com>
2024-03-29 04:09:08 -0300

    feat: add header component

---
+++ buildbox-challenge/app/components/header/headerSyles.ts
@@ -0,0 +1,11 @@
+"use client"
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+    background-color:#2b2b2b;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.5rem 0;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.4);
+`
