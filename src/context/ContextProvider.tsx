478acf323a842cfa491fb372d9f2284978c6e401
cleitson <cleitson.ftw@gmail.com>
2024-03-29 13:32:28 -0300

    adicionando img para os posts iniciais

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -11,16 +11,19 @@ const posts = [
     id: "2024-03-29T16:11:47.197Z",
     name: "Nome do Usuário",
     message: "Loren nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.",
+    image: "/public/photo-base.jpg"
   },
   {
     id: "2024-03-29T16:11:09.747Z",
     name: "Nome do Usuário",
     message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis.",
+    image: "/public/photo-base2.jpg"
   },
   {
     id: "2024-03-29T16:11:01.252Z",
     name: "Manuela Oliveira",
     message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis ligula vel velit scelerisque iaculis. Nam mattis justo id orci commodo, eu tempus purus cursus.",
+    image: "/public/photo-base3.jpg"
   }
 ]
 
