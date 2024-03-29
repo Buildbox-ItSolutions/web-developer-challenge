cc02921171e3f5195b4d4f1cec8569410dd34204
RubsRafa <rubiarafaela2000@gmail.com>
2024-03-29 09:16:46 -0300

    feat: add header component

---
+++ challenge/src/components/Header/style.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderBox = styled.header`
+  min-width: 100%;
+  max-height: 5.813rem;
+  
+  padding-block: 1.5rem;
+  
+  background-color: #2B2B2B;
+
+  img {
+    max-width: 6.438rem;
+    max-height: 2.813rem;
+    
+    margin-inline: auto;
+  }
+`;
