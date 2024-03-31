cfd3bf336881ba3f31085b61f3effbd17e285b79
Luan rodrigues <rodrigues.luan20@gmail.com>
2024-03-31 19:00:22 -0300

    feat: first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Teste</h1>
+    </main>
+  );
+}
