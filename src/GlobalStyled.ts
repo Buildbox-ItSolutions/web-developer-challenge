fac746c8895cb565e63cc50e9fd5345958534835
Ju Nunes <jucilenenunes10@gmail.com>
2024-03-30 22:39:07 -0300

    Organizando GlobalStyled

--- src/GlobalStyled.ts
+++ src/GlobalStyled.ts
@@ -2,19 +2,12 @@ import { createGlobalStyle } from "styled-components";
 import theme from "./Theme";
 
 export const GlobalStyled = createGlobalStyle`
-
-
 :root {
   font-family: Roboto;
   line-height: 1.5;
   font-weight: 300;
-
+  font-size: 14px;
   color: ${theme.colors.text} !important;
-}
-
-@media (prefers-color-scheme: light) {
-  :root {
-    background-color: ${theme.colors.black};
-  }
+  background-color: ${theme.colors.black};
 }
 `;
