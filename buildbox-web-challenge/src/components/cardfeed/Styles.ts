2bd1f59484a6bfdf33832fda94051fddb25815a0
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-01 18:05:43 -0300

    Feat: adiciona estilização do card do feed

---
+++ buildbox-web-challenge/src/components/cardfeed/Styles.ts
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 516px;
+    height: 225px;
+    margin: 8px 425px 16px;
+    padding: 12px 12px 32px 24px;
+    border-radius: 3px;
+    border: solid 1px #3b3b3b;
+    color: #3b3b3b;
+    display: flex;
+    flex-direction: column;
+`;
+
+// export const ContainerBotao = styled.div`
+//     width: 20px;
+//     height: 20px;
+// `;
+
+export const BotaoRemover = styled.img`
+    width: 20px;
+    height: 20px;
+    margin: 0px 12px 24px 454px;
+    object-fit: contain;
+`;
+
+export const ConteudoPost = styled.div`
+    display: flex;
+    width: 100%;
+    padding: 12px;
+`;
+
+export const Foto = styled.img`
+    width: 88px;
+    height: 88px;
+    margin: 24px 32px 16px 0;
+    object-fit: contain;
+    border-radius: 36px;
+`;
+
+export const Textos = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+`;
+
+export const TextoPost = styled.span`
+    width: 330px;
+    height: 80px;
+    margin: 24px 12px 24px 32px;
+    padding-right: 20px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    text-align: left;
+    color: #9f9f9f;
+`;
+
+/* export const Container = styled.div`; */
+
+export const AutorPost = styled.span`
+    padding: 0 0 12px 0;
+`; 
+
