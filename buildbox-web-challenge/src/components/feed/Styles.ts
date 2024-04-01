0100caeef94c808ae2a4ed517426909ca79e2c67
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-01 18:07:22 -0300

    Feat: adiciona estilização para o título do feed

---
+++ buildbox-web-challenge/src/components/feed/Styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Titulo = styled.span`
+    width: 30px;
+    height: 17px;
+    margin: 56px 911px 8px 425px;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: left;
+    color: #7a7a7a;
+`;
