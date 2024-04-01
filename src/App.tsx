4e25c04cd51372c08559df26d7935c46702d9daa
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-03-31 21:51:25 -0300

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FeedPage from './features/Feed/pages/FeedPage';
+import Navbar from './features/General/Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <FeedPage/>
+    </>
+  )
+}
+
+export default App;
