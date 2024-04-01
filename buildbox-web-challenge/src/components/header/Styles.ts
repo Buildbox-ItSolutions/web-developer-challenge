ff4599de9ac8b81a7a3c8d937453ec1e0a1d500b
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-01 15:09:25 -0300

    Feat: adiciona estilização ao header e logo no header

---
+++ buildbox-web-challenge/src/components/header/Styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    width: 100%;
+    height: 93px;
+    padding: 24px 631px 24px 632px;
+    background-color: #2b2b2b;
+`;
+
+export const Logo = styled.img`
+    width: 103px;
+    height: 45px;
+    object-fit: contain;
+`;
