b19a69cd4063658379332cb610ef198e8ace03e0
Mariano Silva <mariano.silva@ufvjm.edu.br>
2024-03-31 21:52:31 -0300

    feat: navbar completed

---
+++ src/features/Feed/pages/FeedPage.style.tsx
@@ -0,0 +1,2 @@
+import styled from "styled-components"
+export const FeedPageStyled = styled.div``
---
+++ src/features/Feed/pages/FeedPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FeedPageStyled } from './FeedPage.style'
+
+function FeedPage() {
+  return (
+    <FeedPageStyled>FeedPage</FeedPageStyled>
+  )
+}
+
+export default FeedPage
