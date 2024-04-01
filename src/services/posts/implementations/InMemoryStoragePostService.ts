a012261f0fd54fa6bd3f3a06280f1fe50184cfdf
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-01 18:47:12 -0300

    test: add in memory post service simulation

---
+++ src/services/posts/implementations/InMemoryStoragePostService.ts
@@ -0,0 +1,56 @@
+import { Post } from '../../../@types/Post';
+import {
+  createPostRequest,
+  CreatePostRequest,
+  deletePostRequest,
+  DeletePostRequest,
+  getPostsQuery,
+  GetPostsQuery,
+} from '../../../contracts/postContracts';
+import { wait } from '../../../utils/wait';
+import { IPostService } from '../IPostService';
+
+const USE_SIMULATION_DELAY = true;
+
+async function processDelay() {
+  if (USE_SIMULATION_DELAY) {
+    await wait();
+  }
+}
+
+export class InMemoryPostService implements IPostService {
+  private posts: Post[] = [];
+
+  async getPosts(query: GetPostsQuery): Promise<Post[]> {
+    const { limit, page } = getPostsQuery.parse(query);
+
+    await processDelay();
+
+    return this.posts.reverse().slice((page - 1) * limit, page * limit);
+  }
+
+  async createPost(dto: CreatePostRequest): Promise<Post> {
+    const { name, message, imageURL } = createPostRequest.parse(dto);
+
+    await processDelay();
+
+    const post: Post = {
+      id: String(this.posts.length + 1),
+      name,
+      message,
+      imageURL,
+    };
+
+    this.posts.push(post);
+
+    return post;
+  }
+
+  async deletePost(dto: DeletePostRequest): Promise<void> {
+    const { id } = deletePostRequest.parse(dto);
+
+    await processDelay();
+
+    this.posts = this.posts.filter((post: Post) => post.id !== id);
+  }
+}
--- src/services/posts/index.ts
+++ src/services/posts/index.ts
@@ -1,4 +1,5 @@
-import { LocalStoragePostService } from './implementations/LocalStoragePostService';
+import { InMemoryPostService } from './implementations/InMemoryStoragePostService';
 import { IPostService } from './IPostService';
 
-export const postService: IPostService = new LocalStoragePostService();
+// export const postService: IPostService = new LocalStoragePostService();
+export const postService: IPostService = new InMemoryPostService();
