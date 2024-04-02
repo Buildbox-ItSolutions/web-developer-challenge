942c4abd14e3a6988e77ad2e1a1e59817736d143
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-02 15:56:00 -0300

    Feat: adiciona funções de pegar os valores dos campos e submetê-los

--- buildbox-web-challenge/src/components/cardpost/CardPost.tsx
+++ buildbox-web-challenge/src/components/cardpost/CardPost.tsx
@@ -1,26 +1,49 @@
 import CardPostButtons from "../cardpostbuttons/CardPostButtons"
-import { Campos, Container, ContainerFoto, Foto, Mensagem, Nome, TextoMensagem, TextoNome } from "./Styles"
+import { Campos, Container, ContainerFoto, Foto, Mensagem, Nome } from "./Styles"
 import PhotoUpload from "../../assets/image@3x.png";
+import { SetStateAction, useState } from "react";
 
 const CardPost = () => {
+    const [nome, setNome] = useState('');
+    const [mensagem, setMensagem] = useState('');
+
+    const handleNomeChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setNome(event.target.value);
+    };
+
+    const handleMensagemChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setMensagem(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        const novoPost = {
+            id: Math.random(),
+            foto: PhotoUpload, 
+            texto: mensagem,
+            autor: nome
+        };
+
+        console.log("Novo post:", novoPost);
+
+        setNome('');
+        setMensagem('');
+    }
+
+    const handleDiscard = () => {
+        setNome('');
+        setMensagem('');
+    }
+
     return (
         <Container>
             <ContainerFoto>
                 <Foto src={PhotoUpload} alt="" />
             </ContainerFoto>
             <Campos>
-                <Nome>
-                    <TextoNome>
-                        Digite seu nome
-                    </TextoNome>
-                </Nome>
-                <Mensagem>
-                    <TextoMensagem>
-                        Mensagem
-                    </TextoMensagem>
-                </Mensagem>
+                <Nome type="text" value={nome} onChange={handleNomeChange} placeholder="Digite seu nome" />
+                <Mensagem value={mensagem} onChange={handleMensagemChange} rows={4} placeholder="Mensagem" />
             </Campos>
-            <CardPostButtons />
+            <CardPostButtons onSubmit={handleSubmit} onDiscard={handleDiscard}/>
         </Container>
     )
 }
