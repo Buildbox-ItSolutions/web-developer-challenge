760f0745beadd7ab0509cbbd4cb7e1671cf8d096
Fernando <fe.bonifacio@hotmail.com>
2024-04-02 16:43:07 -0300

    feat: created boots

---
+++ blog/src/components/Buttons/buttonPublish.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 98px;
+    height: 41px;
+    margin: 0 0 0 24px;
+    padding: 12px 24px;
+    border-radius: 8px;
+    background-color: var(--dark-lime-green);
+    cursor: pointer;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: center;
+    color: white;
+    border: none;
+`;
+
+export default function ButtonPublish() {
+    return (
+        <Button>Publicar</Button>
+    );
+}
---
+++ blog/src/components/Buttons/buttonTrash.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Button = styled.span`
+    width: 60px;
+    height: 17px;
+    margin: 12px 24px 12px 0;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: center;
+    color: var(--brownish-grey);
+
+    cursor: pointer;
+    text-decoration: underline;
+`;
+
+export default function ButtonTrash() {
+    return (
+        <Button>Descartar</Button>
+    );
+}
