ec7318602757c1b8590b05343c8a75b11951f452
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-02 18:45:15 -0300

    Feat: Math.random no id do post, e foto de exemplo para não ser necessário incluir foto

--- buildbox-web-challenge/src/components/cardpost/CardPost.tsx
+++ buildbox-web-challenge/src/components/cardpost/CardPost.tsx
@@ -1,6 +1,7 @@
 import CardPostButtons from "../cardpostbuttons/CardPostButtons"
 import { Campos, Container, ContainerFoto, Foto, Mensagem, Nome } from "./Styles"
 import PhotoUpload from "../../assets/image@3x.png";
+import fotoExemplo from "../../assets/photo-base@3x.png"
 import { SetStateAction, useState } from "react";
 
 const CardPost = () => {
@@ -15,19 +16,34 @@ const CardPost = () => {
         setMensagem(event.target.value);
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
+        const novoId = Math.floor(Math.random() * 1000000);
         const novoPost = {
-            id: Math.random(),
-            foto: PhotoUpload, 
+            id: novoId,
+            foto: fotoExemplo,
             texto: mensagem,
             autor: nome
         };
-
-        console.log("Novo post:", novoPost);
-
-        setNome('');
-        setMensagem('');
-    }
+        try {
+            const response = await fetch('http://localhost:4000/posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(novoPost),
+            });
+            if (!response.ok) {
+                throw new Error('Erro ao enviar post');
+            }
+        
+            setNome('');
+            setMensagem('');
+            alert('Post enviado com sucesso!');
+        } catch (error) {
+            console.error('Erro ao enviar novo post:', error);
+            alert('Erro ao enviar o post. Por favor, tente novamente.');
+        }
+    };
 
     const handleDiscard = () => {
         setNome('');
@@ -43,7 +59,7 @@ const CardPost = () => {
                 <Nome type="text" value={nome} onChange={handleNomeChange} placeholder="Digite seu nome" />
                 <Mensagem value={mensagem} onChange={handleMensagemChange} rows={4} placeholder="Mensagem" />
             </Campos>
-            <CardPostButtons onSubmit={handleSubmit} onDiscard={handleDiscard}/>
+            <CardPostButtons onSubmit={handleSubmit} onDiscard={handleDiscard} />
         </Container>
     )
 }
