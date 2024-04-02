558e0f22a4966552271661ca9d2d79c2ddb7661f
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-02 15:58:15 -0300

    Fix(Style): margin, text-align, cursor

--- buildbox-web-challenge/src/components/cardpostbuttons/Styles.ts
+++ buildbox-web-challenge/src/components/cardpostbuttons/Styles.ts
@@ -2,20 +2,25 @@ import styled from "styled-components";
 
 export const Botoes = styled.div`
     justify-content: flex-end;
-    align-items: end;
+    align-items: flex-end;
+    padding-bottom: 12px;
+    margin-bottom: 20px;
 `;
 
-export const Publicar = styled.span`
+export const Publicar = styled.button`
     width: 98px;
     height: 41px;
-    margin: 0 0 0 24px;
+    margin: 0 0 40px 24px;
     padding: 12px 24px;
     border-radius: 8px;
     background-color: #5f5f5f;
     color: #313131;
+    text-align: center;
+    border: none;
+    cursor: pointer;
 `;
 
-export const Descartar = styled.span`
+export const Descartar = styled.button`
     width: 60px;
     height: 17px;
     margin: 12px 24px 12px 0;
@@ -28,4 +33,5 @@ export const Descartar = styled.span`
     letter-spacing: normal;
     text-align: center;
     color: #5f5f5f;
+    cursor: pointer;
 `;
