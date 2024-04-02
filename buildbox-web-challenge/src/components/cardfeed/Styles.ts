9f29c9b27df6ffae6fa0b314683d6cedc672cc2d
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-02 15:54:15 -0300

    Fix: position e outras estilizações

--- buildbox-web-challenge/src/components/cardfeed/Styles.ts
+++ buildbox-web-challenge/src/components/cardfeed/Styles.ts
@@ -1,68 +1,80 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 516px;
-    height: 225px;
-    margin: 8px 425px 16px;
-    padding: 12px 12px 32px 24px;
-    border-radius: 3px;
-    border: solid 1px #3b3b3b;
-    color: #3b3b3b;
-    display: flex;
-    flex-direction: column;
+  width: 516px;
+  height: 225px;
+  margin: 8px 425px 16px;
+  padding: 32px 24px;
+  padding-top: 56px;
+  border-radius: 3px;
+  border: solid 1px #3b3b3b;
+  color: #3b3b3b;
+  display: flex;
+  position: relative;
+  gap: 32px;
 `;
-
-// export const ContainerBotao = styled.div`
-//     width: 20px;
-//     height: 20px;
-// `;
-
 export const BotaoRemover = styled.img`
-    width: 20px;
-    height: 20px;
-    margin: 0px 12px 24px 454px;
-    object-fit: contain;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  object-fit: contain;
 `;
 
 export const ConteudoPost = styled.div`
-    display: flex;
-    width: 100%;
-    padding: 12px;
+  display: flex;
+  width: 100%;
+  gap: 32px;
 `;
 
 export const Foto = styled.img`
-    width: 88px;
-    height: 88px;
-    margin: 24px 32px 16px 0;
-    object-fit: contain;
-    border-radius: 36px;
+  width: 88px;
+  height: 88px;
+  object-fit: contain;
+  border-radius: 36px;
 `;
 
 export const Textos = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  flex: 1;
 `;
 
 export const TextoPost = styled.span`
-    width: 330px;
-    height: 80px;
-    margin: 24px 12px 24px 32px;
-    padding-right: 20px;
-    font-family: Roboto;
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.25;
-    letter-spacing: normal;
-    text-align: left;
-    color: #9f9f9f;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  line-height: 1.25;
+  letter-spacing: normal;
+  text-align: left;
+  color: #9f9f9f;
 `;
 
 /* export const Container = styled.div`; */
 
-export const AutorPost = styled.span`
-    padding: 0 0 12px 0;
-`; 
+export const PostInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1.33;
+    letter-spacing: 0.29px;
 
+`;
+
+export const Autor = styled.span`
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.29;
+  letter-spacing: normal;
+  text-align: left;
+  color: #7a7a7a;
+`;
