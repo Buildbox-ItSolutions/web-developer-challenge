3aa5174022ec2cb2eebf26c46c0c3c46ee346618
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-04-02 09:48:01 -0300

    feat: add animations for posts

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
@@ -6,6 +7,20 @@ import { PostCard } from '../PostCard';
 import { PostCardSkeleton } from '../PostCard/skeleton';
 
 import * as S from './styles';
+
+const itemAnimationVariant = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -20 },
+};
+
+const defaultItemAnimationProps = {
+  variants: itemAnimationVariant,
+  initial: 'hidden',
+  animate: 'visible',
+  exit: 'exit',
+  layout: true,
+};
 
 export function PostsList() {
   const { ref, inView } = useInView();
@@ -28,44 +43,50 @@ export function PostsList() {
   if (isLoadingPosts) {
     return (
       <S.UlStyled>
-        <li>
+        <motion.li {...defaultItemAnimationProps}>
           <PostCardSkeleton />
-        </li>
-        <li>
+        </motion.li>
+        <motion.li {...defaultItemAnimationProps}>
           <PostCardSkeleton />
-        </li>
+        </motion.li>
       </S.UlStyled>
     );
   }
 
   return (
     <S.UlStyled>
-      {noPosts && (
-        <S.EmptyPostsListFeedback>
-          Não há nenhum post cadastrado no momento.
-        </S.EmptyPostsListFeedback>
-      )}
+      <AnimatePresence>
+        {noPosts && (
+          <S.EmptyPostsListFeedback>
+            Não há nenhum post cadastrado no momento.
+          </S.EmptyPostsListFeedback>
+        )}
 
-      {posts.map((post, index) => {
-        const isLast = index === posts.length - 1;
+        {posts.map((post, index) => {
+          const isLast = index === posts.length - 1;
 
-        return (
-          <li key={post.id} ref={isLast ? ref : undefined}>
-            <PostCard post={post} />
-          </li>
-        );
-      })}
+          return (
+            <motion.li
+              key={post.id}
+              ref={isLast ? ref : undefined}
+              {...defaultItemAnimationProps}
+            >
+              <PostCard post={post} />
+            </motion.li>
+          );
+        })}
 
-      {isFetchingNextPage && (
-        <>
-          <li>
-            <PostCardSkeleton />
-          </li>
-          <li>
-            <PostCardSkeleton />
-          </li>
-        </>
-      )}
+        {isFetchingNextPage && (
+          <>
+            <motion.li>
+              <PostCardSkeleton />
+            </motion.li>
+            <motion.li>
+              <PostCardSkeleton />
+            </motion.li>
+          </>
+        )}
+      </AnimatePresence>
     </S.UlStyled>
   );
 }
