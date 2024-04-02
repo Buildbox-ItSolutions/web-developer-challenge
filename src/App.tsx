28d835e4886e7883d5f2bebd78190f025a3249e1
Bryan Fernandes <bryanfernandes8@gmail.com>
2024-04-01 23:07:08 -0300

    fix: upload same image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { PostContextProvider } from "./contexts/postContext";
 import Feed from "./sections/feed";
 import NewPost from "./sections/newPost";
-import { AppContainer, AppContent } from "./styles";
-
+import { AppContainer, AppContent, AppHeader } from "./styles";
+import buildBoxImg from "./assets/bx-logo.svg"
 
 function App() {
-
 
   return (
     <PostContextProvider>
       <AppContainer>
+        <AppHeader>
+          <img src={buildBoxImg} alt="build box web challenge image" />
+        </AppHeader>
         <AppContent>
           <NewPost />
           <Feed />
--- src/styles.ts
+++ src/styles.ts
@@ -13,12 +13,24 @@ const AppContent = styled.div`
     gap: 4rem;
     width: 500px;
 
+    padding: 8rem 0;
+
     @media screen and (max-width: 600px) {
         width: 350px;
     } 
 `
 
+const AppHeader = styled.div`
+    position: fixed;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: #2b2b2b;
+    padding: 1.5rem 0;
+`
+
 export {
     AppContainer,
-    AppContent
+    AppContent,
+    AppHeader
 }
