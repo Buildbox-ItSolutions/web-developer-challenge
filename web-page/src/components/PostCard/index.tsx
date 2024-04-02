6e564d064f9b53a988d976470dc13d5194e97ef2
Bruno-carneiro-da-silva <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-04-02 02:51:49 -0300

    :boom: fix: unused code

--- web-page/src/components/PostCard/index.tsx
+++ web-page/src/components/PostCard/index.tsx
@@ -1,7 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { AxiosError } from "axios";
 import React from "react";
-import { FormProvider, set, useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import * as Yup from 'yup';
 import logo from "../../assets/images/logo.svg";
