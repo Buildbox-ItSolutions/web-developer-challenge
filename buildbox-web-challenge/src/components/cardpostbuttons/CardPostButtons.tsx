04eeb7e1e0cb7c8b5cc100d99067f70101c73742
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-02 15:57:35 -0300

    Feat: adiciona funções para os botões do componente, diferindo o que cada um irá fazer

--- buildbox-web-challenge/src/components/cardpostbuttons/CardPostButtons.tsx
+++ buildbox-web-challenge/src/components/cardpostbuttons/CardPostButtons.tsx
@@ -1,16 +1,29 @@
 import { Botoes, Descartar, Publicar } from "./Styles"
+import ICardPostButtons from '../../interfaces/ICardPosrButtons';
 
-const CardPostButtons = () => {
+const CardPostButtons: React.FC<ICardPostButtons> = ({ onSubmit, onDiscard }) => {
+    const handlePublish = () => {
+        if (window.confirm('Tem certeza de que deseja publicar este post?')) {
+            onSubmit();
+        }
+    };
+
+    const handleDiscard = () => {
+        if (window.confirm('Tem certeza de que deseja descartar as alterações?')) {
+            onDiscard();
+        }
+    };
+
     return (
         <Botoes>
-            <Descartar>
+            <Descartar onClick={handleDiscard}>
                 Descartar
             </Descartar>
-            <Publicar>
+            <Publicar onClick={handlePublish}>
                 Publicar
             </Publicar>
         </Botoes>
-    )
-}
+    );
+};
 
-export default CardPostButtons
+export default CardPostButtons;
