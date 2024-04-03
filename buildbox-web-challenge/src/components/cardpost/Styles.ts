2e7f5e881f1240c1a76b6d631fbeb5b8a64ca7b4
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-03 16:06:46 -0300

    Fix(Style): tamanhos, padding, margin e outros componentes

--- buildbox-web-challenge/src/components/cardpost/Styles.ts
+++ buildbox-web-challenge/src/components/cardpost/Styles.ts
@@ -3,11 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 516px;
     height: 353px;
-    margin: 40px 425px 56px;
+    margin: 40px auto 56px;
     padding: 24px;
     border-radius: 3px;
     border: solid 1px #3b3b3b;
     background-color: #313131;
+
+    @media (max-width: 400px) {
+        width: 330px;
+        height: 300px;
+        padding: 24px;
+        margin: 25px auto 56px;
+    }
 `;
 
 export const ContainerFoto = styled.div`
@@ -19,12 +26,29 @@ export const ContainerFoto = styled.div`
     border-radius: 36px;
     border: solid 1px #4b4b4b;
     background-color: rgba(75, 75, 75, 0);
+
+    @media (max-width: 400px) {
+        width: 44px;
+        height: 44px;
+        margin: 0 auto;
+        padding: 32px;
+        object-fit: contain;
+        border-radius: 36px;
+        border: solid 1px #4b4b4b;
+        background-color: rgba(75, 75, 75, 0);
+    }
 `;
 
 export const Foto = styled.img`
     width: 24px;
     height: 24px;
     object-fit: contain;
+
+    @media (max-width: 400px) {
+        width: 20px;
+        height: 20px;
+        margin-bottom: 50px;
+    }
 `;
 
 export const Campos = styled.div`
@@ -44,6 +68,13 @@ export const Nome = styled.input`
     font-size: 14px;
     font-weight: normal;
     outline: none;
+
+    @media (max-width: 400px) {
+        width: 300px;
+        height: 35px;
+        margin: 16px auto 8px;
+        padding: 12px 1rem 12px 1rem;
+    }
 `;
 
 export const Mensagem = styled.textarea`
@@ -59,4 +90,10 @@ export const Mensagem = styled.textarea`
     border: none;
     font-size: 14px;
     font-weight: normal;
+
+    @media (max-width: 400px) {
+        width: 300px;
+        height: 35px;
+        scroll: hidden;
+    }
 `;
