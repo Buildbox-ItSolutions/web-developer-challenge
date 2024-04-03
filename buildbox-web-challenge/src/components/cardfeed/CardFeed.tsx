fe8ef8309a20fc9011f98c47b343e3445f56a594
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-03 14:48:31 -0300

    Feat: adiciona a ação de remover o post

--- buildbox-web-challenge/src/components/cardfeed/CardFeed.tsx
+++ buildbox-web-challenge/src/components/cardfeed/CardFeed.tsx
@@ -1,32 +1,38 @@
 import {
-    Container,
-    Foto,
-    TextoPost,
-    Textos,
-    BotaoRemover,
-    ConteudoPost,
-    PostInfos,
+  Container,
+  Foto,
+  TextoPost,
+  Textos,
+  BotaoRemover,
+  ConteudoPost,
+  PostInfos
 } from "./Styles";
 import Remover from "../../assets/delete@3x.png";
 import ICardFeed from "../../interfaces/ICardFeed";
-  
-  const CardFeed = ({ id, foto, texto, autor }: ICardFeed) => {
-    return (
-      <Container>
-        <BotaoRemover src={Remover} alt='' />
-        <ConteudoPost key={id}>
-          <Foto src={foto} alt='' />
-          <Textos>
-            <TextoPost>
-              {texto}
-            </TextoPost>
-            <PostInfos>
-              Enviado por <span>{autor}</span>
-            </PostInfos>
-          </Textos>
-        </ConteudoPost>
-      </Container>
-    );
-  };
-  
-  export default CardFeed;
+
+interface Props extends ICardFeed {
+  onRemove: () => void;
+}
+
+const CardFeed: React.FC<Props> = ({ id, foto, texto, autor, onRemove }) => {
+  return (
+    <Container>
+      <BotaoRemover onClick={onRemove}>
+        <img src={Remover} alt="" />
+      </BotaoRemover>
+      <ConteudoPost key={id}>
+        <Foto src={foto} alt='' />
+        <Textos>
+          <TextoPost>
+            {texto}
+          </TextoPost>
+          <PostInfos>
+            Enviado por <span>{autor}</span>
+          </PostInfos>
+        </Textos>
+      </ConteudoPost>
+    </Container>
+  );
+};
+
+export default CardFeed;
