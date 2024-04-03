790b733d15ece73f17efdf3d502983028b550b06
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-03 16:08:02 -0300

    Fix(Style): ajusta para o mobile

--- buildbox-web-challenge/src/components/header/Styles.ts
+++ buildbox-web-challenge/src/components/header/Styles.ts
@@ -3,12 +3,24 @@ import styled from "styled-components";
 export const Container = styled.header`
     width: 100%;
     height: 93px;
-    padding: 24px 631px 24px 632px;
+    padding: 24px auto 632px;
     background-color: #2b2b2b;
+
+    @media (max-width: 516px) {
+        width: 100%;
+        height: 90px;
+    }
 `;
 
 export const Logo = styled.img`
     width: 103px;
     height: 45px;
+    margin: 0 auto;
     object-fit: contain;
+
+    @media (max-width: 516) {
+        width: 103px;
+        height: 45px;
+        margin: 50% 50%;
+    }
 `;
