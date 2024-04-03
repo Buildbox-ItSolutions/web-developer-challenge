4ad2095528d9450d64c2ccd4ac5891db5e61455b
Diego Chagas <diegofchagas@gmail.com>
2024-04-03 15:57:28 -0300

    feature: add estilações globais e alterando estrutura do body

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Header } from "./components/Header"
-
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
 function App() {
-  
-
   return (
     <>
-    <Header/>
+      <Header />
+      <div className="post-container">
+        <section>
+          <Post />
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/globalStyle.tsx
+++ src/globalStyle.tsx
@@ -11,6 +11,25 @@ export const GlobalStyle = createGlobalStyle`
 body{
   background-color:#343434;
   font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+a,button,textarea,input{
+  font-family: "Roboto", sans-serif;
+}
+.post-container{
+  display:flex;
+  justify-content:center;
+  h3{
+  font-size:.875rem;
+  color:#7a7a7a;
+  line-height:18px;
+  margin-bottom:8px;
+  font-weight:500;
+}
+  
+}
+section{
+margin-top: 2.5rem;
 }
 
-`
+`;
