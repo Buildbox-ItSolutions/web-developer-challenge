c381a65c15c3f4112778f7d3c131b6cc24afdaaf
LuisCetauro <cetaurot@gmail.com>
2024-04-03 18:03:25 -0300

    Update Connect.js

--- webchallenge/src/app/lib/Connect.js
+++ webchallenge/src/app/lib/Connect.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 const connection = {};
 
-const URI =
-  "mongodb+srv://cetauro:FUuhBa97REEyfX7A@cluster0.urr8sjm.mongodb.net/next?retryWrites=true&w=majority&appName=Cluster0";
-
 export const connectToDb = async () => {
   try {
     if (connection.isConnected) {
       return;
     }
-    const db = await mongoose.connect(URI);
+    const db = await mongoose.connect(process.env.URI);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
