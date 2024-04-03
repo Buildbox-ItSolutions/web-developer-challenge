83ca65139565bb100f4f52761c22b87faeb0928b
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-04-03 18:20:04 -0300

    Fix: corrige posicionamento do logo

--- buildbox-web-challenge/src/components/header/Styles.ts
+++ buildbox-web-challenge/src/components/header/Styles.ts
@@ -5,10 +5,14 @@ export const Container = styled.header`
     height: 93px;
     padding: 24px auto 632px;
     background-color: #2b2b2b;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     @media (max-width: 516px) {
         width: 100%;
         height: 90px;
+        padding: 24px auto 632px;
     }
 `;
 
@@ -21,6 +25,5 @@ export const Logo = styled.img`
     @media (max-width: 516) {
         width: 103px;
         height: 45px;
-        margin: 50% 50%;
     }
 `;
