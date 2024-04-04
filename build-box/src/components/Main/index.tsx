fc08034a20f3332451547b4f8d22d44315c57fc1
iago <iago.pferreiravr@gmail.com>
2024-04-04 18:53:59 -0300

    refactor: Main component to use Feed component

--- build-box/src/components/Main/index.tsx
+++ build-box/src/components/Main/index.tsx
@@ -1,24 +1,12 @@
 import './Main.css';
 import Form from '../Form';
-import Post from '../Post';
-import posts from '../../assets/data/posts';
+import Feed from '../Feed';
 
 function Main() {
 	return (
 		<main>
 			<Form />
-			<section>
-				<h3>Feed</h3>
-				{posts.map(({ image, message, author, id }) => (
-					<Post
-						image={image}
-						message={message}
-						author={author}
-						key={id}
-						uuid={id}
-					/>
-				))}
-			</section>
+			<Feed />
 		</main>
 	);
 }
