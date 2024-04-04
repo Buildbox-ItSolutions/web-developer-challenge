58eaf5186dd6d9435f5f3a6ce6e22a3264ba9c65
iago <iago.pferreiravr@gmail.com>
2024-04-04 13:18:30 -0300

    test: Adicionando configurações para possibilitar testes com RTL

--- build-box/vite.config.ts
+++ build-box/vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './setupTests.ts',
+		css: true,
+		reporters: ['verbose'],
+		coverage: {
+			reporter: ['text', 'json', 'html'],
+			provider: 'v8',
+		},
+	},
 });
