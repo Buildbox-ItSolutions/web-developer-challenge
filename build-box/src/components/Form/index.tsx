ceb86b3a6b485e5f2367a8507b14446b24c57dcc
iago <iago.pferreiravr@gmail.com>
2024-04-04 17:00:27 -0300

    refactor: add useForm hook to Form component

--- build-box/src/components/Form/index.tsx
+++ build-box/src/components/Form/index.tsx
@@ -1,7 +1,18 @@
-import './Main.css';
+import './Form.css';
 import photograph from '../../assets/photograph.svg';
+import { useForm } from './hooks/useForm';
 
 function Form() {
+	const {
+		image,
+		message,
+		name,
+		setImage,
+		setMessage,
+		setName,
+		discard,
+		publish,
+	} = useForm();
 	return (
 		<form action="">
 			<label htmlFor="file-input">
@@ -10,21 +21,41 @@ function Form() {
 					id="file-input"
 					type="file"
 					data-testid="image-input"
+					onChange={({ target: { value } }) => setImage(value)}
+					value={image}
 				/>
 				<img src={photograph} alt="" />
 			</label>
 			<label htmlFor="name-input">
-				<input type="text" id="name-input" data-testid="name-input" />
+				<input
+					type="text"
+					id="name-input"
+					data-testid="name-input"
+					onChange={({ target: { value } }) => setName(value)}
+					value={name}
+				/>
 			</label>
 			<label htmlFor="message-input">
 				<textarea
 					name="message"
 					id="message-input"
 					data-testid="message-input"
+					onChange={({ target: { value } }) => setMessage(value)}
+					value={message}
 				/>
 			</label>
-			<button data-testid="discard-button">Descartar</button>
-			<button type="submit" data-testid="publish-button">
+			<button
+				type="submit"
+				data-testid="discard-button"
+				onClick={(e) => discard(e)}
+			>
+				Descartar
+			</button>
+			<button
+				type="submit"
+				data-testid="publish-button"
+				onClick={(e) => publish(e)}
+			>
 				Publicar
 			</button>
 		</form>
