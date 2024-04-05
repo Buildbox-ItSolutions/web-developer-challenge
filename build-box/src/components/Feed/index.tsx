a122e936ee709fcd15c3121a875059a7a8fe8b19
iago <iago.pferreiravr@gmail.com>
2024-04-05 17:52:12 -0300

    style: refactor Feed component styling

--- build-box/src/components/Feed/index.tsx
+++ build-box/src/components/Feed/index.tsx
@@ -1,13 +1,40 @@
 import { useContext } from 'react';
 import Post from '../Post';
 import { Context } from '../Main/context';
+import styled from 'styled-components';
+
+const StylizedTitle = styled.h3`
+	color: #7a7a7a;
+	font-family: 'roboto', 'sans-serif';
+	font-size: 14px;
+	font-stretch: normal;
+	font-style: normal;
+	font-weight: 500;
+	height: 17px;
+	letter-spacing: normal;
+	line-height: 1.29;
+	margin-bottom: 8px;
+	text-align: left;
+	width: 30px;
+`;
+
+const StylizedFeed = styled.section`
+	height: 100%;
+	margin: auto;
+	margin-top: 145px;
+	width: 516px;
+
+	@media screen and (max-width: 515px) {
+		width: calc(100% - 24px);
+	}
+`;
 
 function Feed() {
 	const { deletePost, mainPosts } = useContext(Context);
 
 	return (
-		<section>
-			<h3>Feed</h3>
+		<StylizedFeed>
+			<StylizedTitle>Feed</StylizedTitle>
 			{mainPosts.map(({ image, message, author, id }) => (
 				<Post
 					image={image}
@@ -18,7 +45,7 @@ function Feed() {
 					handleClick={() => deletePost(id)}
 				/>
 			))}
-		</section>
+		</StylizedFeed>
 	);
 }
 
