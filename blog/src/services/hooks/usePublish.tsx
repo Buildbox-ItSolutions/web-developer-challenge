87db3c841ab094d43bf2ff9020ce9f78b91ab3b4
Fernando <fe.bonifacio@hotmail.com>
2024-04-11 10:19:13 -0300

    feat: creating array to store posts

--- blog/src/services/hooks/usePublish.tsx
+++ blog/src/services/hooks/usePublish.tsx
@@ -4,24 +4,28 @@ export const handlePublish = () => {
     const name = (document.getElementById('input-name') as HTMLInputElement).value;
     const description = (document.getElementById('input-description') as HTMLInputElement).value;
     const inputImage = document.getElementById('image-upload') as HTMLInputElement;
-    
-    
+
     if (inputImage.files && inputImage.files[0]) {
         const reader = new FileReader();
-        
+
         reader.onload = function(event) {
             const imagePath = event.target?.result as string;
 
             if (name && description && imagePath) {
-                localStorage.setItem('cardName', name);
-                localStorage.setItem('cardDescription', description);
-                localStorage.setItem('cardImagePath', imagePath);
+                const publication = { name, description, imagePath };
+                let publications = JSON.parse(localStorage.getItem('publications') || '[]');
+                publications.push(publication);
+                localStorage.setItem('publications', JSON.stringify(publications));
+
+                (document.getElementById('input-name') as HTMLInputElement).value = '';
+                (document.getElementById('input-description') as HTMLInputElement).value = '';
+                (document.getElementById('image-upload') as HTMLInputElement).value = '';
+                (document.getElementById('image-preview') as HTMLInputElement).src = '';
             } else {
                 alert('Preencha todos os campos!');
             }
         };
 
-        
         reader.readAsDataURL(inputImage.files[0]);
     } else {
         alert('Selecione uma imagem!');
