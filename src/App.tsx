b26ed3a2210c94c14cb2dee5d068798d8c38a5dd
Wesley-wsl <wesleywestelley@gmail.com>
2024-06-20 16:05:35 -0300

    feat: apply theme and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "./core/styles/theme";
+import { GlobalStyles } from "./core/styles/global";
+import { Main } from "./pages/Main";
 
 function App() {
-  return <div />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Main />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
