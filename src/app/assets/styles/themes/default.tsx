ed60094363cdf485d30f803e59cad72741dc7a8a
Victor Figueiredo <vcfo081@gmail.com>
2024-07-04 01:28:56 -0300

    fix: add global styles

---
+++ src/app/assets/styles/themes/default.tsx
@@ -0,0 +1,30 @@
+const defaultTheme = {
+  colors: {
+    background: "#343434",
+    inputBackground: "#494949",
+    primary: {
+      main: "#71BB00",
+    },
+    secondary: {
+      main: "#d65923",
+    },
+    gray: {
+      lightest: "#9F9F9F",
+      lighter: "#4b4b4b",
+      light: "#3B3B3B",
+      main: "#313131",
+      dark: "#2b2b2b",
+      darker: "#232323",
+    },
+    // danger: {
+    //   main: "#FC5050",
+    //   light: "#F97171",
+    //   dark: "#FF0000",
+    // },
+    // success: {
+    //   main: "#51CA73",
+    // },
+  },
+};
+
+export { defaultTheme };
