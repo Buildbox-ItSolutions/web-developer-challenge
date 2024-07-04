c834366e51a5ffa853d6bab03ee8234cd69128a2
prtpj1 <prtpj1@gmail.com>
2024-07-04 17:10:51 -0300

    :memo: Starting the buildbox-web-challenge adds initial npm packs adds css reset file removes initial vite stuffs

---
+++ buildbox-posts-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './Styles/App.css'
+import {useForm} from 'react-hook-form';
+import styled from 'styled-components';
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
