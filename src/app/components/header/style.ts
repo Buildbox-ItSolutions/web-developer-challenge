b2b5fe1283333e7e6a241402b6d1a255157cc797
Victor Figueiredo <vcfo081@gmail.com>
2024-07-04 02:03:07 -0300

    fix: text size in header component

--- src/app/components/header/style.ts
+++ src/app/components/header/style.ts
@@ -5,18 +5,18 @@ export const HeaderContainer = styled.nav`
   height: 93px;
   background: ${(props) => props.theme.colors.gray.dark};
   text-align: center;
-  padding-top: 20px;
+  padding-top: 22px;
   border-bottom: 1px solid ${(props) => props.theme.colors.gray.darker};
 `;
 
 export const Title = styled.h1`
-  font-size: 28px;
+  font-size: 24px;
   font-weight: 900;
   color: ${(props) => props.theme.colors.primary.main};
 `;
 
 export const Subtitle = styled.h2`
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 300;
   color: ${(props) => props.theme.colors.gray.lightest};
 `;
