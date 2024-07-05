1111ab5abb43f094d1314a6a0379f3af48b1896b
Victor Figueiredo <vcfo081@gmail.com>
2024-07-04 22:58:59 -0300

    feat: pending deletion in Posts component

--- src/app/posts/model.tsx
+++ src/app/posts/model.tsx
@@ -1,5 +1,7 @@
-import { getPosts } from "@/api";
-import { useQuery } from "@tanstack/react-query";
+import { deletePost, getPosts } from "@/api";
+import { queryClient } from "@/lib/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { IPost } from "../types";
 
 export default function usePostsModel() {
   const { data: posts, isLoading } = useQuery({
@@ -7,8 +9,28 @@ export default function usePostsModel() {
     queryFn: getPosts,
   });
 
+  const {
+    mutateAsync: deletePostFn,
+    isPending,
+    variables,
+  } = useMutation({
+    mutationFn: deletePost,
+    onSuccess: (_, variables) => {
+      const cached: IPost[] | undefined = queryClient.getQueryData(["posts"]);
+      const newPosts = cached?.filter((post) => post.id !== variables);
+      queryClient.setQueryData(["posts"], newPosts);
+    },
+  });
+
+  const handleDelete = (id: number) => {
+    deletePostFn(id);
+  };
+
   return {
     isLoading,
+    isPending,
+    variables,
     posts,
+    handleDelete,
   };
 }
--- src/app/posts/view.tsx
+++ src/app/posts/view.tsx
@@ -10,7 +10,10 @@ import { SkeletonLoader } from "../components/loader";
 
 export default function PostsView({
   isLoading,
+  isPending,
+  variables,
   posts,
+  handleDelete,
 }: ReturnType<typeof usePostsView>) {
   return (
     <>
@@ -21,9 +24,22 @@ export default function PostsView({
           {isLoading && <SkeletonLoader />}
           {posts
             ?.map(({ id, avatar, name, message }: IPost) => (
-              <Post key={id} avatar={avatar} name={name} message={message} />
+              <Post
+                key={id}
+                id={id}
+                avatar={avatar}
+                name={name}
+                message={message}
+                handleDelete={handleDelete}
+                isPending={isPending && variables === id}
+              />
             ))
             .reverse()}
+          {posts?.length === 0 && !isLoading && (
+            <h1 style={{ textAlign: "center", marginTop: "30px" }}>
+              Não há mensagens
+            </h1>
+          )}
         </List>
       </Container>
     </>
