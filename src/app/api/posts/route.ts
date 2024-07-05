62034212c71edc6281805cb12ef048e1747e2854
Victor Figueiredo <vcfo081@gmail.com>
2024-07-04 22:57:03 -0300

    feat: add delete post api route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -22,3 +22,22 @@ export async function POST(req: NextRequest) {
 
   return NextResponse.json({ posts }, { status: 201 });
 }
+
+export async function DELETE(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get("id");
+
+  if (!id)
+    return NextResponse.json({ error: "Id is required" }, { status: 400 });
+
+  const posts = await getData();
+
+  const index = posts.findIndex((post) => post.id === parseInt(id));
+
+  if (index === -1) {
+    return NextResponse.json({ error: "Post not found" }, { status: 404 });
+  }
+
+  posts.splice(index, 1);
+
+  return NextResponse.json({ posts }, { status: 200 });
+}
