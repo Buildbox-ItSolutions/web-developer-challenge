d333bba6311a71ae52a84399c32a30ad837d0203
Victor Figueiredo <vcfo081@gmail.com>
2024-07-04 21:09:33 -0300

    feat: form component features and MVVM

--- src/app/components/form/model.tsx
+++ src/app/components/form/model.tsx
@@ -1,42 +1,102 @@
 "use client";
 
-import { IPostPayload } from "@/app/types";
+import { IPost, IPostPayload } from "@/app/types";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
+import * as yup from "yup";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createPost } from "@/api";
 
-const postSchema = Yup.object().shape({
-  avatar: Yup.string(),
-  name: Yup.string()
+const schema = yup.object().shape({
+  avatar: yup.string(),
+  name: yup
+    .string()
     .min(3, "O nome precisa ter no mínimo 3 carácteres")
     .max(50, "Tente utilizar no máximo 50 carácteres")
     .required("Preencha seu nome"),
-  message: Yup.string()
+  message: yup
+    .string()
     .min(3, "A mensagem precisa ter no mínimo 3 carácteres")
     .max(280, "Tente utilizar no máximo 280 carácteres")
-    .required("Email is required"),
+    .required("Digite a mensagem"),
 });
 
 export const useFormModel = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  const onSubmit = () => {};
-
+  const queryClient = useQueryClient();
   const {
-    register,
+    control,
     handleSubmit,
-    formState: { errors },
-  } = useForm<IPostPayload>({
-    resolver: yupResolver(postSchema),
+    watch,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm<IPostPayload>({ resolver: yupResolver(schema) });
+
+  const avatarImage = watch("avatar");
+
+  const { mutateAsync: createPostFn } = useMutation({
+    mutationFn: createPost,
+    onSuccess: (_, variables) => {
+      const cached: IPost[] | undefined = queryClient.getQueryData(["posts"]);
+      const newPost = {
+        ...variables,
+        id: cached ? cached.length + 1 : 1,
+      } as IPost;
+      queryClient.setQueryData(["posts"], (data: IPost[]) => [
+        ...data,
+        newPost,
+      ]);
+      resetForm();
+    },
   });
-  console.log("errors");
-  console.log(errors);
+
+  const onSubmit = async (data: IPostPayload) => {
+    try {
+      await createPostFn(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const resetForm = () => {
+    resetAvatar();
+    setValue("name", "");
+    setValue("message", "");
+  };
+
+  const resetAvatar = () => setValue("avatar", "");
+
+  const handleImageUpload = (file: React.ChangeEvent<HTMLInputElement>) => {
+    const fileCollected = file.target.files?.[0];
+    if (!fileCollected) return "";
+    const img = new Image();
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      if (!ctx) return "";
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+
+      const base64Image = canvas.toDataURL(fileCollected.type);
+      setValue("avatar", base64Image, { shouldValidate: true });
+    };
+
+    img.src = URL.createObjectURL(fileCollected);
+  };
+
   return {
-    isLoading,
     handleSubmit,
-    register,
+    control,
     onSubmit,
     errors,
+    isValid,
+    handleImageUpload,
+    avatarImage,
+    resetAvatar,
+    resetForm,
   };
 };
