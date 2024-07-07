07a1da6c36d0bc85339278e853f87417c7a2fd70
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-07-07 19:01:35 -0300

    feat(style): form style with styled-components

---
+++ src/components/Form-style.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+    padding: 15px 0;
+
+    border-radius: 5px;
+    border: 1px solid #353535;
+    background-color: var(--bg-section-color);
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+
+    input, textarea {
+        padding: 10px;
+        width: 90%;
+        color: #fff;
+        background-color: var(--form-input-data);
+
+        border-radius: 10px;
+        margin-bottom: 10px;
+        border: none;
+    }
+
+    textarea {
+        min-width: 90%;
+        max-width: 90%;
+        min-height: 60px;
+        max-height: 150px;
+    }
+
+    input::placeholder, textarea::placeholder {
+        color: #9F9F9F;
+    }
+
+    input:focus, textarea:focus {
+        outline: none;
+    }
+
+    .buttons-wrapper {
+        width: 90%;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+
+        margin-top: 35px;
+    }
+
+    a {
+        color: #494949;
+        margin-right: 10px;
+    }
+
+    button {
+        background-color: #5F5F5F;
+        border: none;
+        border-radius: 8px;
+        padding: 12px;
+        display: inline;
+        
+        color: #343434;
+        width: 20%;
+
+    }
+`;
--- src/pages/feed/Feed.tsx
+++ src/pages/feed/Feed.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <main>
       <Form />
+
     </main>
   )
 }
