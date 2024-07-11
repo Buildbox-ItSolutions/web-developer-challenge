888d444691f9594dc1b681939bd6ed1d331bf822
thayvitareli <thainyvitareli@hotmail.com>
2024-07-11 18:26:05 -0300

    refactor  - header styled components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,9 @@
 import React from "react"
-import styled from "styled-components"
 import logo from '../../assets/logo.jpg';
+import { StyledHeader, StyledImage } from "./style";
 
 export default function Header(){
-    const StyledHeader = styled.div`
-        width: 100%;
-        background-color: #2B2B2B;
-        height: 75px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    `;
-
-    const StyledHeaderText = styled.p`
-        color: #71BB00;
-    `;
-    const StyledImage = styled.img`
-        width: 20;
-        height: 50px;
-    `;
-
+    
     return <StyledHeader>
                 <StyledImage src={logo}/>
         </StyledHeader>
---
+++ src/components/header/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+    width: 100%;
+    background-color: #2B2B2B;
+    height: 75px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledHeaderText = styled.p`
+    color: #71BB00;
+`;
+
+const StyledImage = styled.img`
+    width: 20;
+    height: 50px;
+`;
+
+export {
+    StyledHeader,
+    StyledHeaderText,
+    StyledImage
+}
