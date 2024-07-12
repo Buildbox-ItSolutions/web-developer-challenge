f9be718a4bb3bd233a9fe5ec8a861720146f579d
andre <andrealbuquerque2001@gmail.com>
2024-07-12 12:07:09 -0300

    feat: create component getPost

--- challenge/src/components/post/getPost.tsx
+++ challenge/src/components/post/getPost.tsx
@@ -1,46 +1,68 @@
 'use client'
-import { Post } from '@/action/post'
+import { usePostStatus } from '@/hook/postStatusContext'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { GoXCircle } from 'react-icons/go'
+import { Delete } from './delete'
+import { getPosts } from '@/function/getPost'
+
+type PostType = {
+  image: string
+  name: string
+  message: string
+  id: string
+}
 
 export const GetPost = () => {
-  const [posts, setPosts] = useState([])
+  const { postAdded, setPostAdded } = usePostStatus()
+  const [posts, setPosts] = useState<PostType[]>([])
 
   useEffect(() => {
-    const postInstance = new Post()
-    const allPosts = postInstance.GetPosts()
-    if (allPosts) {
-      setPosts(allPosts)
-    }
+    const data = getPosts()
+    setPosts(data)
+    setPostAdded(false)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    if (postAdded) {
+      const data = getPosts()
+      setPosts(data)
+      setPostAdded(false)
+    }
+  }, [postAdded, setPostAdded])
   return (
-    <div className="max-w-[600px] mx-auto mt-10 text-zinc-500">
+    <div className="max-w-[600px] mx-auto mt-10 text-zinc-500 transition duration-500">
       <p>Feed</p>
-      <div className="min-h-52 bg-zinc-750 max-w-[600px] mx-auto  border border-zinc-600 rounded-md flex flex-col">
-        <GoXCircle className="w-5 h-5 self-end mr-4 mt-2 font-thin text-trash cursor-pointer" />
-        <div className="flex items-start w-[90%] justify-around">
-          <Image
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrNj_60Q6bbDcOR27keAVB7M1-2xEqxjaOCw&usqp=CAU"
-            width={120}
-            height={120}
-            alt="Imagem do user"
-            className="w-28 h-28 rounded-[40px]"
-          />
-          <div className="w-[70%] space-y-5">
-            <p className="text-zinc-400 text-justify">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Deserunt, necessitatibus. Aliquid neque veniam sapiente quos qui
-              consectetur ad natus reprehenderit nam modi eveniet nemo molestias
-              laboriosam dicta, cupiditate pariatur cum?
-            </p>
-            <div className="">
-              <p className="text-xs text-zinc-550">Enviado por</p>
-              <p className="text-sm text-zinc-500 mb-8">Manuela Oliveira</p>
+      <div className="space-y-4 mt-2">
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post, index) => (
+            <div
+              className="h-auto bg-zinc-750 max-w-[600px] mx-auto border border-zinc-600 rounded-md flex flex-col"
+              key={index}
+            >
+              <Delete id={post.id} />
+              <div className="flex items-start w-full justify-around mt-2">
+                <Image
+                  src={post.image}
+                  alt="Imagem do post"
+                  width={120}
+                  height={120}
+                  className="w-24 h-24 rounded-[40px] mb-4"
+                />
+                <div className="w-[70%] max-md:w-[60%] space-y-5 pr-4">
+                  <p className="text-zinc-400 text-justify text-wrap w-full h-auto break-words">
+                    {post.message}
+                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  </p>
+                  <div className="">
+                    <p className="text-xs text-zinc-550">Enviado por</p>
+                    <p className="text-sm text-zinc-500 mb-4">{post.name}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </div>
   )
