ae80786347ae8a5a4b87ef7fe6167b7c8c0b30f2
andre <andrealbuquerque2001@gmail.com>
2024-07-12 12:08:03 -0300

    feat: update page

--- challenge/src/app/page.tsx
+++ challenge/src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import { CreatePost } from '@/components/post/create'
 import { GetPost } from '@/components/post/getPost'
+import { PostStatusProvider } from '@/hook/postStatusContext'
 
 export default function Home() {
   return (
-    <div>
-      <CreatePost />
-      <GetPost />
+    <div className="px-2">
+      <PostStatusProvider>
+        <CreatePost />
+        <GetPost />
+      </PostStatusProvider>
     </div>
   )
 }
